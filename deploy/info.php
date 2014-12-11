d1e7a9891c015097e1f9813c8d5d8b28551fd443
pcbaldwin <pbaldwin@clearfoundation.com>
2014-12-11 14:41:50 -0500

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'policy_manager';
-$app['version'] = '2.0.1';
+$app['version'] = '2.0.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-policy-manager.spec
+++ packaging/app-policy-manager.spec
@@ -1,7 +1,7 @@
 
 Name: app-policy-manager
 Epoch: 1
-Version: 2.0.1
+Version: 2.0.5
 Release: 1%{dist}
 Summary: Policy Manager
 License: Proprietary
