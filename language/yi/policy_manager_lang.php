3e96f279f485ac69494cc5ee2f80b41bc479f8bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:40:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'די אַללוסערס גרופּע איז אַ ספּעציעל גרופּע געניצט צו שפּור אַלע די ניצערס אויף דעם סיסטעם. ביטע שאַפֿן אַ נייַ גרופּע אויב איר דאַרפֿן צו ספּעציפיצירן אַ סאַבסעט פון ניצערס אויף דעם סיסטעם.';
+$lang['policy_manager_app_description'] = 'די פּאָליטיק מאַנאַגער אַפּ גיט די מאָטאָר פֿאַר דיפּלויינג ספּעציפיש אַפּ פּאַלאַסיז צו ספּעציפיש גרופּעס.';
+$lang['policy_manager_app_invalid'] = 'אַפּ איז פאַרקריפּלט.';
+$lang['policy_manager_app_name'] = 'פּאָליטיק מאַנאַגער';
+$lang['policy_manager_description_invalid'] = 'באַשרייַבונג איז פאַרקריפּלט.';
+$lang['policy_manager_group'] = 'גרופּע';
+$lang['policy_manager_group_invalid'] = 'גרופּע איז פאַרקריפּלט.';
+$lang['policy_manager_policies'] = 'פּאָליסיעס';
+$lang['policy_manager_policy'] = 'פּאָליטיק';
+$lang['policy_manager_policy_already_exists'] = 'פּאָליטיק עקזיסטירט שוין.';
+$lang['policy_manager_policy_id_invalid'] = 'פּאָליטיק שייַן איז פאַרקריפּלט.';
+$lang['policy_manager_policy_name'] = 'פּאָליטיק נאמען';
+$lang['policy_manager_policy_name_invalid'] = 'פּאָליטיק נאָמען איז פאַרקריפּלט.';
+$lang['policy_manager_policy_not_found'] = 'פּאָליטיק ניט געפֿונען.';
+$lang['policy_manager_priority_invalid'] = 'בילכערקייַט איז פאַרקריפּלט.';
