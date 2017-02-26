b27541409e7d9196fb03d5c2d89109771f602209
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'AllUsers ჯგუფის სპეციალური ჯგუფი, რომელიც გამოიყენება აკონტროლოთ ყველა მომხმარებელი სისტემაზე. გთხოვთ, შექმნათ ახალი ჯგუფი, თუ თქვენ უნდა მიუთითოთ subset მომხმარებლებს სისტემა.';
+$lang['policy_manager_app_description'] = 'პოლიტიკის მენეჯერი app უზრუნველყოფს ძრავის შემოყვანა კონკრეტული app პოლიტიკის კონკრეტული ჯგუფები.';
+$lang['policy_manager_app_invalid'] = 'App არასწორია.';
+$lang['policy_manager_app_name'] = 'პოლიტიკის მენეჯერი';
+$lang['policy_manager_description_invalid'] = 'აღწერა არასწორია.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'ჯგუფი არასწორია.';
+$lang['policy_manager_policies'] = 'პოლიტიკა';
+$lang['policy_manager_policy'] = 'პოლიტიკა';
+$lang['policy_manager_policy_already_exists'] = 'პოლიტიკა უკვე არსებობს.';
+$lang['policy_manager_policy_id_invalid'] = 'პოლიტიკა ID არასწორია.';
+$lang['policy_manager_policy_name'] = 'პოლიტიკის სახელი';
+$lang['policy_manager_policy_name_invalid'] = 'პოლიტიკა სახელი არასწორია.';
+$lang['policy_manager_policy_not_found'] = 'პოლიტიკა არ არის ნაპოვნი.';
+$lang['policy_manager_priority_invalid'] = 'პრიორიტეტი არასწორია.';
