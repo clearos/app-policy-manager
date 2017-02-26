b632720cc0a677aef4e21b099a45f513def479f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Golongan allusers nyaeta grup husus dipaké pikeun lagu sadaya pamaké dina sistem. Mangga nyieun grup anyar upami Anjeun kudu nangtukeun hiji sawaréh pamaké dina sistem.';
+$lang['policy_manager_app_description'] = 'Nu Kawijakan Manager app nyadiakeun engine pikeun deploying kawijakan app husus pikeun grup husus.';
+$lang['policy_manager_app_invalid'] = 'App téh sah.';
+$lang['policy_manager_app_name'] = 'kawijakan Manager';
+$lang['policy_manager_description_invalid'] = 'Description téh sah.';
+$lang['policy_manager_group'] = 'kumpulan';
+$lang['policy_manager_group_invalid'] = 'Grup anu sah.';
+$lang['policy_manager_policies'] = 'kawijakan';
+$lang['policy_manager_policy'] = 'kawijakan';
+$lang['policy_manager_policy_already_exists'] = 'Kawijakan geus nyampak.';
+$lang['policy_manager_policy_id_invalid'] = 'ID kawijakan anu sah.';
+$lang['policy_manager_policy_name'] = 'kawijakan Ngaran';
+$lang['policy_manager_policy_name_invalid'] = 'ngaran kawijakan anu sah.';
+$lang['policy_manager_policy_not_found'] = 'Kawijakan teu kapanggih.';
+$lang['policy_manager_priority_invalid'] = 'Prioritas anu sah.';
