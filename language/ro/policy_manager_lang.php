23d882f825919aa3e20267f42c855f8b21dd3db5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Gruparea allusers este un grup special folosit pentru a urmări toți utilizatorii sistemului. Vă rugăm să creați un nou grup în cazul în care aveți nevoie pentru a specifica un subset de utilizatori pe sistem.';
+$lang['policy_manager_app_description'] = 'Aplicația Politica Manager oferă motorului pentru implementarea unor politici specifice de aplicații pentru grupuri specifice.';
+$lang['policy_manager_app_invalid'] = 'App este nevalid.';
+$lang['policy_manager_app_name'] = 'Managerul de politică';
+$lang['policy_manager_description_invalid'] = 'Descrierea este nevalid.';
+$lang['policy_manager_group'] = 'grup';
+$lang['policy_manager_group_invalid'] = 'Group este nevalid.';
+$lang['policy_manager_policies'] = 'politicile';
+$lang['policy_manager_policy'] = 'Politică';
+$lang['policy_manager_policy_already_exists'] = 'Există deja politică.';
+$lang['policy_manager_policy_id_invalid'] = 'ID-ul de politică este nevalid.';
+$lang['policy_manager_policy_name'] = 'Nume Politica';
+$lang['policy_manager_policy_name_invalid'] = 'Numele politicii este nevalid.';
+$lang['policy_manager_policy_not_found'] = 'Politica nu a fost găsit.';
+$lang['policy_manager_priority_invalid'] = 'Prioritate este nevalid.';
