b08af684e5b9c64f9014ec888e454e648abc306f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:40:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'I allusers qembu iqembu elikhethekile esetshenziswa ukulandelela bonke abasebenzisi ohlelweni. Sicela udale iqembu elisha uma udinga ukucacisa subset abasebenzisi ohlelweni.';
+$lang['policy_manager_app_description'] = 'Uhlelo lokusebenza Manager yemfihlo inikeza injini for zokuphaka izinqubomgomo lokusebenza ethize ukuze amaqembu athile.';
+$lang['policy_manager_app_invalid'] = 'App ayivumelekile.';
+$lang['policy_manager_app_name'] = 'Umphathi yemfihlo';
+$lang['policy_manager_description_invalid'] = 'Incazelo ayivumelekile.';
+$lang['policy_manager_group'] = 'Iqembu';
+$lang['policy_manager_group_invalid'] = 'Group ayivumelekile.';
+$lang['policy_manager_policies'] = 'Izinqubomgomo';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy selivele likhona.';
+$lang['policy_manager_policy_id_invalid'] = 'ID yemfihlo ayivumelekile.';
+$lang['policy_manager_policy_name'] = 'Igama yemfihlo';
+$lang['policy_manager_policy_name_invalid'] = 'Igama yemfihlo alivumelekile.';
+$lang['policy_manager_policy_not_found'] = 'Policy ayitholakalanga.';
+$lang['policy_manager_priority_invalid'] = 'Okumqoka kwesi ayivumelekile.';
