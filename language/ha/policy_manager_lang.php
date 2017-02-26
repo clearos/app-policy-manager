fa57bdf9af027b6add3b69db25465096ec8436f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers kungiyar ne na musamman kungiyar amfani da su waƙa da duk masu amfani a kan tsarin. Don Allah haifar da wani sabon rukuni idan kana bukatar ka saka a tsarin cikin tsari na masu amfani a kan tsarin.';
+$lang['policy_manager_app_description'] = 'The Policy Manager app bayar da engine for tura musamman app manufofin zuwa takamaiman kungiyoyin.';
+$lang['policy_manager_app_invalid'] = 'App ba daidai ba ne.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Description ba daidai ba ne.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group ba daidai ba ne.';
+$lang['policy_manager_policies'] = 'Asiri';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy riga ya wanzu.';
+$lang['policy_manager_policy_id_invalid'] = 'Policy ID ba daidai ba ne.';
+$lang['policy_manager_policy_name'] = 'Policy Name';
+$lang['policy_manager_policy_name_invalid'] = 'Policy sunan ba daidai ba ne.';
+$lang['policy_manager_policy_not_found'] = 'Policy ba su samu.';
+$lang['policy_manager_priority_invalid'] = 'Fifiko ba daidai ba ne.';
