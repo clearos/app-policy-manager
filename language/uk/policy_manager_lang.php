1d5f4bca058710ad58d8b66278cdb5d60b25683e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Група AllUsers спеціальна група, яка використовується для відстеження всіх користувачів системи. Будь ласка, створіть нову групу, якщо вам потрібно вказати підмножину користувачів в системі.';
+$lang['policy_manager_app_description'] = 'Додаток диспетчер політик забезпечує механізм для розгортання конкретної політики додатків для конкретних груп.';
+$lang['policy_manager_app_invalid'] = 'App є недійсним.';
+$lang['policy_manager_app_name'] = 'Менеджер по політиці';
+$lang['policy_manager_description_invalid'] = 'Опис недійсний.';
+$lang['policy_manager_group'] = 'група';
+$lang['policy_manager_group_invalid'] = 'Група є недійсним.';
+$lang['policy_manager_policies'] = 'поліси';
+$lang['policy_manager_policy'] = 'політика';
+$lang['policy_manager_policy_already_exists'] = 'Політика вже існує.';
+$lang['policy_manager_policy_id_invalid'] = 'Політика ID недійсний.';
+$lang['policy_manager_policy_name'] = 'ім&#39;я політики';
+$lang['policy_manager_policy_name_invalid'] = 'Ім&#39;я політики є недійсним.';
+$lang['policy_manager_policy_not_found'] = 'Політика не знайдений.';
+$lang['policy_manager_priority_invalid'] = 'Пріоритет є недійсним.';
