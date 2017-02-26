f2900ec16f743e727aa03b6bc18d7bc5edd28900
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Den allusers gruppen er en spesiell gruppe som brukes til å spore alle brukere på systemet. Opprett en ny gruppe hvis du trenger å angi en undergruppe av brukere på systemet.';
+$lang['policy_manager_app_description'] = 'Den Policy Manager app gir motoren for distribusjon av spesifikke app politikk til bestemte grupper.';
+$lang['policy_manager_app_invalid'] = 'App er ugyldig.';
+$lang['policy_manager_app_name'] = 'policy manager';
+$lang['policy_manager_description_invalid'] = 'Beskrivelse er ugyldig.';
+$lang['policy_manager_group'] = 'Gruppe';
+$lang['policy_manager_group_invalid'] = 'Konsernet er ugyldig.';
+$lang['policy_manager_policies'] = 'politikk';
+$lang['policy_manager_policy'] = 'Politikk';
+$lang['policy_manager_policy_already_exists'] = 'Regler finnes allerede.';
+$lang['policy_manager_policy_id_invalid'] = 'Regler ID er ugyldig.';
+$lang['policy_manager_policy_name'] = 'Policynavn';
+$lang['policy_manager_policy_name_invalid'] = 'Regler for navn er ugyldig.';
+$lang['policy_manager_policy_not_found'] = 'Politikk ikke funnet.';
+$lang['policy_manager_priority_invalid'] = 'Prioritet er ugyldig.';
