cb6d914e53e3697385d735aeff9c066da97e5f80
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:28:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'مجموعة ALLUSERS هي مجموعة خاصة تستخدم لتتبع جميع المستخدمين على النظام. يرجى إنشاء مجموعة جديدة إذا كنت بحاجة إلى تحديد مجموعة فرعية من المستخدمين على النظام.';
+$lang['policy_manager_app_description'] = 'يوفر تطبيق مدير سياسة المحرك لنشر سياسات التطبيق محددة لفئات محددة.';
+$lang['policy_manager_app_invalid'] = 'التطبيق غير صالح.';
+$lang['policy_manager_app_name'] = 'مدير السياسة';
+$lang['policy_manager_description_invalid'] = 'وصف غير صالح.';
+$lang['policy_manager_group'] = 'مجموعة';
+$lang['policy_manager_group_invalid'] = 'مجموعة غير صالحة.';
+$lang['policy_manager_policies'] = 'سياسات';
+$lang['policy_manager_policy'] = 'سياسات';
+$lang['policy_manager_policy_already_exists'] = 'سياسة موجود بالفعل.';
+$lang['policy_manager_policy_id_invalid'] = 'معرف سياسة غير صالح.';
+$lang['policy_manager_policy_name'] = 'اسم السياسة';
+$lang['policy_manager_policy_name_invalid'] = 'اسم النهج غير صالح.';
+$lang['policy_manager_policy_not_found'] = 'سياسة غير موجود.';
+$lang['policy_manager_priority_invalid'] = 'الأولوية هي غير صالحة.';
