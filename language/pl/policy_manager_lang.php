c2067990260b6d98ba62b24ffdb492790efb0071
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Grupa ALLUSERS jest grupa specjalna wykorzystywane do śledzenia wszystkich użytkowników w systemie. Proszę utworzyć nową grupę, jeśli chcesz określić podzbiór użytkowników w systemie.';
+$lang['policy_manager_app_description'] = 'Aplikacja Policy Manager zapewnia silnik dla wdrażania konkretnych polityk aplikacji do konkretnych grup.';
+$lang['policy_manager_app_invalid'] = 'App jest nieprawidłowy.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Opis jest nieprawidłowy.';
+$lang['policy_manager_group'] = 'Grupa';
+$lang['policy_manager_group_invalid'] = 'Grupa jest nieprawidłowy.';
+$lang['policy_manager_policies'] = 'polityk';
+$lang['policy_manager_policy'] = 'Polityka';
+$lang['policy_manager_policy_already_exists'] = 'Zasady już istnieje.';
+$lang['policy_manager_policy_id_invalid'] = 'ID Polityka jest nieprawidłowy.';
+$lang['policy_manager_policy_name'] = 'Nazwa Polityka';
+$lang['policy_manager_policy_name_invalid'] = 'Nazwa zasady jest nieprawidłowy.';
+$lang['policy_manager_policy_not_found'] = 'Nie znaleziono polityki.';
+$lang['policy_manager_priority_invalid'] = 'Priorytet jest nieprawidłowy.';
