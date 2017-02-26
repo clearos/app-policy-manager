e7b32fd5e36676a3ac6b1ad2b2e634dbecf9b35b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Skupina allusers je špeciálna skupina slúži na sledovanie všetkých užívateľov v systéme. ak je potrebné zadať podmnožinu používateľov v systéme Prosím vytvoriť novú skupinu.';
+$lang['policy_manager_app_description'] = 'Aplikácia Policy Manager poskytuje motor pre nasadenie špecifické politiky aplikácie so špecifickými skupinami.';
+$lang['policy_manager_app_invalid'] = 'App je neplatný.';
+$lang['policy_manager_app_name'] = 'Policy manager';
+$lang['policy_manager_description_invalid'] = 'Popis je neplatný.';
+$lang['policy_manager_group'] = 'skupina';
+$lang['policy_manager_group_invalid'] = 'Skupina je neplatný.';
+$lang['policy_manager_policies'] = 'Postupy';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika už existuje.';
+$lang['policy_manager_policy_id_invalid'] = 'Politika ID je neplatné.';
+$lang['policy_manager_policy_name'] = 'Názov zásady';
+$lang['policy_manager_policy_name_invalid'] = 'Názov zásady je neplatný.';
+$lang['policy_manager_policy_not_found'] = 'Politika nebola nájdená.';
+$lang['policy_manager_priority_invalid'] = 'Priorita je neplatný.';
