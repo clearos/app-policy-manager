dbec95e8f98b38655522dfee863a0803a237317a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers குழு கணினியில் அனைத்து பயனர்கள் கண்காணிக்க பயன்படுத்தப்படும் ஒரு சிறப்பு குழு உள்ளது. நீங்கள் கணினியில் பயனர்கள் ஒரு துணைக்குழு குறிப்பிட வேண்டும் என்றால், ஒரு புதிய குழுவை உருவாக்க கொள்ளவும்.';
+$lang['policy_manager_app_description'] = 'கொள்கை மேலாளர் பயன்பாட்டை குறிப்பிட்ட குழுக்கள் குறிப்பிட்ட பயன்பாட்டை கொள்கைகள் வரிசைப்படுத்துவது இயந்திரம் வழங்குகிறது.';
+$lang['policy_manager_app_invalid'] = 'ஆப் தவறானது.';
+$lang['policy_manager_app_name'] = 'கொள்கை மேலாளர்';
+$lang['policy_manager_description_invalid'] = 'விளக்கம் தவறானது.';
+$lang['policy_manager_group'] = 'குழு';
+$lang['policy_manager_group_invalid'] = 'குழு தவறானது.';
+$lang['policy_manager_policies'] = 'கொள்கைகள்';
+$lang['policy_manager_policy'] = 'கொள்கை';
+$lang['policy_manager_policy_already_exists'] = 'கொள்கை ஏற்கனவே உள்ளது.';
+$lang['policy_manager_policy_id_invalid'] = 'கொள்கை ஐடி தவறானது.';
+$lang['policy_manager_policy_name'] = 'கொள்கைப் பெயர்';
+$lang['policy_manager_policy_name_invalid'] = 'கொள்கை பெயர் செல்லுபடியாகாது.';
+$lang['policy_manager_policy_not_found'] = 'கொள்கை காணப்படவில்லை.';
+$lang['policy_manager_priority_invalid'] = 'முன்னுரிமை தவறானது.';
