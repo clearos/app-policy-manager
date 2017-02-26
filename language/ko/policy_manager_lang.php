188441903945269a0e8530db683b8a0002ec29b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'ALLUSERS를 그룹은 시스템의 모든 사용자를 추적하는 데 사용되는 특정 그룹이다. 당신이 시스템에 사용자의 하위 집합을 지정해야하는 경우 새 그룹을 만드십시오.';
+$lang['policy_manager_app_description'] = '정책 관리자 응용 프로그램은 특정 그룹에 특정 응용 프로그램 정책을 배포하는 엔진을 제공합니다.';
+$lang['policy_manager_app_invalid'] = '앱이 잘못되었습니다.';
+$lang['policy_manager_app_name'] = '정책 관리자';
+$lang['policy_manager_description_invalid'] = '설명이 잘못되었습니다.';
+$lang['policy_manager_group'] = '그룹';
+$lang['policy_manager_group_invalid'] = '그룹이 잘못되었습니다.';
+$lang['policy_manager_policies'] = '정책';
+$lang['policy_manager_policy'] = '정책';
+$lang['policy_manager_policy_already_exists'] = '정책이 이미 존재합니다.';
+$lang['policy_manager_policy_id_invalid'] = '정책 ID가 잘못되었습니다.';
+$lang['policy_manager_policy_name'] = '정책 이름';
+$lang['policy_manager_policy_name_invalid'] = '정책 이름이 잘못되었습니다.';
+$lang['policy_manager_policy_not_found'] = '정책을 찾을 수 없습니다.';
+$lang['policy_manager_priority_invalid'] = '우선 순위가 잘못되었습니다.';
