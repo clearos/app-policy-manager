7a56536be18e7a7044ce9036fd91cad2e36fde1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Ny vondrona allusers dia vondrona manokana nampiasaina hanaraha-maso ny mpampiasa eo amin&#39;ny rafitra. Azafady mamorona vondrona vaovao raha toa ka mila ny mamaritra ny subset ny mpampiasa eo amin&#39;ny rafitra.';
+$lang['policy_manager_app_description'] = 'Ny Policy Manager fampiharana manome ny maotera naparitaka manokana ho an&#39;ny fampiharana ny politika ho vondrona manokana.';
+$lang['policy_manager_app_invalid'] = 'App dia tsy manan-kery.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Description dia tsy manan-kery.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group dia tsy manan-kery.';
+$lang['policy_manager_policies'] = 'Policies';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy efa misy.';
+$lang['policy_manager_policy_id_invalid'] = 'Policy ID dia tsy manan-kery.';
+$lang['policy_manager_policy_name'] = 'Policy Name';
+$lang['policy_manager_policy_name_invalid'] = 'Policy anarany tsy manan-kery.';
+$lang['policy_manager_policy_not_found'] = 'Policy tsy hita.';
+$lang['policy_manager_priority_invalid'] = 'Laharam-pahamehana dia tsy manan-kery.';
