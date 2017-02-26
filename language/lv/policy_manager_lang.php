383bf23fb9be3caf91d45072b9d0c89955edbd1f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Allusers grupa ir īpaša grupa, ko izmanto, lai izsekotu visus lietotājus par sistēmas. Lūdzu, izveidojiet jaunu grupu, ja jums ir nepieciešams norādīt apakškopu lietotājiem par sistēmu.';
+$lang['policy_manager_app_description'] = 'Politika vadītājs app nodrošina dzinēju izvietot konkrētus lietotņu politiku konkrētām grupām.';
+$lang['policy_manager_app_invalid'] = 'App ir nederīgs.';
+$lang['policy_manager_app_name'] = 'politika vadītājs';
+$lang['policy_manager_description_invalid'] = 'Apraksts ir nederīgs.';
+$lang['policy_manager_group'] = 'grupa';
+$lang['policy_manager_group_invalid'] = 'Grupa ir nederīgs.';
+$lang['policy_manager_policies'] = 'Politikas';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika jau pastāv.';
+$lang['policy_manager_policy_id_invalid'] = 'Politika ID ir nederīgs.';
+$lang['policy_manager_policy_name'] = 'politika Name';
+$lang['policy_manager_policy_name_invalid'] = 'Politikas nosaukums nav derīgs.';
+$lang['policy_manager_policy_not_found'] = 'Politika nav atrasts.';
+$lang['policy_manager_priority_invalid'] = 'Prioritāte ir nederīgs.';
