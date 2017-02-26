7aaee1b3c20cdadc6f538864efb107e0d14d7134
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers گروپ نظام پر تمام صارفین کو ٹریک کرنے کے لئے استعمال ایک خاص گروپ ہے. آپ کے سسٹم پر صارفین کی ایک اپسمچی کی وضاحت کرنے کی ضرورت ہے تو ایک نیا گروپ تشکیل دے براہ مہربانی.';
+$lang['policy_manager_app_description'] = 'پالیسی منیجر اے پی پی کے مخصوص گروپوں کے لئے مخصوص اے پی پی کی پالیسیوں کی تعیناتی کے لئے انجن فراہم کرتا ہے.';
+$lang['policy_manager_app_invalid'] = 'اپلی غلط ہے.';
+$lang['policy_manager_app_name'] = 'پالیسی مینیجر';
+$lang['policy_manager_description_invalid'] = 'تفصیل غلط ہے.';
+$lang['policy_manager_group'] = 'گروپ';
+$lang['policy_manager_group_invalid'] = 'گروپ باطل ہے.';
+$lang['policy_manager_policies'] = 'پالیسیاں';
+$lang['policy_manager_policy'] = 'پالیسی';
+$lang['policy_manager_policy_already_exists'] = 'پالیسی پہلے سے موجود ہے.';
+$lang['policy_manager_policy_id_invalid'] = 'پالیسی ID غلط ہے.';
+$lang['policy_manager_policy_name'] = 'پالیسی نام';
+$lang['policy_manager_policy_name_invalid'] = 'پالیسی کا نام غلط ہے.';
+$lang['policy_manager_policy_not_found'] = 'پالیسی نہیں ملا.';
+$lang['policy_manager_priority_invalid'] = 'ترجیحی غلط ہے.';
