ab9e0321bc90ed9e8cfe96d4f78b37211e9362a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers গ্রুপ একটি বিশেষ সিস্টেমের সকল ব্যবহারকারীদের ট্র্যাক করা হয় গ্রুপ. যদি আপনি সিস্টেম ব্যবহারকারীদের একটি উপসেট নির্দিষ্ট করতে হবে দয়া করে একটি নতুন গ্রুপ তৈরি করুন.';
+$lang['policy_manager_app_description'] = 'নীতি ম্যানেজার অ্যাপ্লিকেশন নির্দিষ্ট গ্রুপ নির্দিষ্ট অ্যাপ নীতি স্থাপনার জন্য ইঞ্জিন উপলব্ধ.';
+$lang['policy_manager_app_invalid'] = 'অ্যাপ অবৈধ.';
+$lang['policy_manager_app_name'] = 'নীতি ম্যানেজার';
+$lang['policy_manager_description_invalid'] = 'বর্ণনা অবৈধ.';
+$lang['policy_manager_group'] = 'গ্রুপ';
+$lang['policy_manager_group_invalid'] = 'গ্রুপ অবৈধ.';
+$lang['policy_manager_policies'] = 'নীতিসমূহ';
+$lang['policy_manager_policy'] = 'নীতি';
+$lang['policy_manager_policy_already_exists'] = 'নীতি ইতিমধ্যেই বিদ্যমান.';
+$lang['policy_manager_policy_id_invalid'] = 'নীতি আইডি অবৈধ.';
+$lang['policy_manager_policy_name'] = 'পলিসি নাম';
+$lang['policy_manager_policy_name_invalid'] = 'নীতি নাম অবৈধ.';
+$lang['policy_manager_policy_not_found'] = 'নীতি পাওয়া যায়নি.';
+$lang['policy_manager_priority_invalid'] = 'অগ্রাধিকার অবৈধ.';
