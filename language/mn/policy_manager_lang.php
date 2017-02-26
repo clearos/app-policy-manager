6b5516cc041099291f9526473ed4653a74525fba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers бүлэг систем дээр бүх хэрэглэгчдийг хянахад ашиглагддаг тусгай бүлэг юм. Хэрэв та систем дээр хэрэглэгч шилж зааж өгөх хэрэгтэй бол шинэ бүлэг бий болгох уу.';
+$lang['policy_manager_app_description'] = 'Бодлогын менежер апп тодорхой бүлгийн онцлог апп бодлого босгох хөдөлгүүртэй болно.';
+$lang['policy_manager_app_invalid'] = 'App хүчин төгөлдөр бус байна.';
+$lang['policy_manager_app_name'] = 'бодлогын менежер';
+$lang['policy_manager_description_invalid'] = 'Тодорхойлолт нь хүчин төгөлдөр бус байна.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group хүчин төгөлдөр бус байна.';
+$lang['policy_manager_policies'] = 'Бодлого';
+$lang['policy_manager_policy'] = 'бодлогын';
+$lang['policy_manager_policy_already_exists'] = 'Бодлогын аль хэдийн байна.';
+$lang['policy_manager_policy_id_invalid'] = 'Бодлогын ID хүчин төгөлдөр бус байна.';
+$lang['policy_manager_policy_name'] = 'бодлогын нэр';
+$lang['policy_manager_policy_name_invalid'] = 'Бодлогын нэр хүчин төгөлдөр бус байна.';
+$lang['policy_manager_policy_not_found'] = 'Бодлогын олдсонгүй.';
+$lang['policy_manager_priority_invalid'] = 'Тэргүүлэх хүчин төгөлдөр бус байна.';
