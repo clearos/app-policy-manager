4161506333b5ed2dde517051ba08f7ca8a590c06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers ಗುಂಪು ವ್ಯವಸ್ಥೆಯಲ್ಲಿ ಎಲ್ಲಾ ಬಳಕೆದಾರರಿಗೆ ಗುರುತಿಸಲು ಬಳಸಲಾಗುತ್ತದೆ ವಿಶೇಷ ಗುಂಪು. ನೀವು ಗಣಕದಲ್ಲಿ ಬಳಕೆದಾರರು ಉಪವಿಭಾಗ ಸೂಚಿಸಲು ಅಗತ್ಯವಿದ್ದರೆ, ದಯವಿಟ್ಟು ಹೊಸ ಗುಂಪೊಂದನ್ನು ಸೃಷ್ಟಿಸುವುದು.';
+$lang['policy_manager_app_description'] = 'ಪಾಲಿಸಿ ಮ್ಯಾನೇಜರ್ ಅಪ್ಲಿಕೇಶನ್ ನಿರ್ದಿಷ್ಟ ಗುಂಪುಗಳಿಗೆ ನಿರ್ದಿಷ್ಟ ಅಪ್ಲಿಕೇಶನ್ ನೀತಿಗಳನ್ನು ಅನುಷ್ಠಾನ ಎಂಜಿನ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['policy_manager_app_invalid'] = 'ಅಪ್ಲಿಕೇಶನ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['policy_manager_app_name'] = 'ಪಾಲಿಸಿ ಮ್ಯಾನೇಜರ್';
+$lang['policy_manager_description_invalid'] = 'ವಿವರಣೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['policy_manager_group'] = 'ಗ್ರೂಪ್';
+$lang['policy_manager_group_invalid'] = 'ಗ್ರೂಪ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['policy_manager_policies'] = 'ನೀತಿಗಳು';
+$lang['policy_manager_policy'] = 'ನೀತಿ';
+$lang['policy_manager_policy_already_exists'] = 'ನೀತಿ ಈಗಾಗಲೇ ಅಸ್ತಿತ್ವದಲ್ಲಿದೆ.';
+$lang['policy_manager_policy_id_invalid'] = 'ನೀತಿ ID ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['policy_manager_policy_name'] = 'ನೀತಿಯ ಹೆಸರು';
+$lang['policy_manager_policy_name_invalid'] = 'ನೀತಿ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['policy_manager_policy_not_found'] = 'ನೀತಿ ಕಂಡು ಬಂದಿಲ್ಲ.';
+$lang['policy_manager_priority_invalid'] = 'ಆದ್ಯತಾ ಅಮಾನ್ಯವಾಗಿದೆ.';
