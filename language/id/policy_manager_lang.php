1684f502a2396ccc9256cc58be890b0f26d5c7ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Kelompok allusers adalah kelompok khusus yang digunakan untuk melacak semua pengguna pada sistem. Harap membuat grup baru jika Anda perlu untuk menentukan subset dari pengguna pada sistem.';
+$lang['policy_manager_app_description'] = 'Kebijakan Manajer aplikasi menyediakan mesin untuk menyebarkan kebijakan aplikasi khusus untuk kelompok tertentu.';
+$lang['policy_manager_app_invalid'] = 'App tidak valid.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Deskripsi tidak valid.';
+$lang['policy_manager_group'] = 'Kelompok';
+$lang['policy_manager_group_invalid'] = 'Kelompok tidak valid.';
+$lang['policy_manager_policies'] = 'kebijakan';
+$lang['policy_manager_policy'] = 'Kebijakan';
+$lang['policy_manager_policy_already_exists'] = 'Kebijakan sudah ada.';
+$lang['policy_manager_policy_id_invalid'] = 'ID kebijakan tidak valid.';
+$lang['policy_manager_policy_name'] = 'Kebijakan Nama';
+$lang['policy_manager_policy_name_invalid'] = 'Nama kebijakan tidak valid.';
+$lang['policy_manager_policy_not_found'] = 'Kebijakan tidak ditemukan.';
+$lang['policy_manager_priority_invalid'] = 'Prioritas tidak valid.';
