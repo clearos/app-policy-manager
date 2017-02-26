661abc42f751002d26b2261b8c3316b22241b277
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'U allusers grupa je posebna grupa koristi za praćenje svim korisnicima na sistemu. Molimo vas da se stvori novu grupu, ako vam je potrebno da navedete podskup korisnika na sistemu.';
+$lang['policy_manager_app_description'] = 'Aplikacija Policy Manager pruža motor za razvijanje specifične politike aplikaciju za određene grupe.';
+$lang['policy_manager_app_invalid'] = 'App je nevažeći.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Opis je nevažeći.';
+$lang['policy_manager_group'] = 'Grupa';
+$lang['policy_manager_group_invalid'] = 'Grupa je nevažeći.';
+$lang['policy_manager_policies'] = 'politike';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika već postoji.';
+$lang['policy_manager_policy_id_invalid'] = 'Politika ID je nevažeći.';
+$lang['policy_manager_policy_name'] = 'Politika ime';
+$lang['policy_manager_policy_name_invalid'] = 'Ime politika je nevažeća.';
+$lang['policy_manager_policy_not_found'] = 'Politika nije pronađen.';
+$lang['policy_manager_priority_invalid'] = 'Prioritet je nevažeći.';
