c2789dc949adff8b7942463be0c80c13786c706f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'ກຸ່ມການກ່າວເຖິງກຸ່ມພິເສດການນໍາໃຊ້ used ທີ່ຈະຕິດຕາມຜູ້ໃຊ້ທັງຫມົດໃນລະບົບ. ກະລຸນາສ້າງກຸ່ມໃຫມ່ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະລະບຸສ່ວນຍ່ອຍຂອງຜູ້ຊົມໃຊ້ໃນລະບົບ.';
+$lang['policy_manager_app_description'] = 'ການ app ນະໂຍບາຍການສະຫນອງເຄື່ອງຈັກສໍາລັບການນໍາໃຊ້ນະໂຍບາຍ app ສະເພາະກຸ່ມສະເພາະໃດຫນຶ່ງ.';
+$lang['policy_manager_app_invalid'] = 'App ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['policy_manager_app_name'] = 'ຜູ້ຈັດການນະໂຍບາຍ';
+$lang['policy_manager_description_invalid'] = 'ລາຍລະອຽດບໍ່ຖືກຕ້ອງ.';
+$lang['policy_manager_group'] = 'ກຸ່ມ';
+$lang['policy_manager_group_invalid'] = 'ກຸ່ມດັ່ງກ່າວແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['policy_manager_policies'] = 'ນະໂຍບາຍ';
+$lang['policy_manager_policy'] = 'ນະໂຍບາຍ';
+$lang['policy_manager_policy_already_exists'] = 'ນະໂຍບາຍທີ່ມີຢູ່ແລ້ວ.';
+$lang['policy_manager_policy_id_invalid'] = 'ID ນະໂຍບາຍບໍ່ຖືກຕ້ອງ.';
+$lang['policy_manager_policy_name'] = 'ຊື່ນະໂຍບາຍ';
+$lang['policy_manager_policy_name_invalid'] = 'ຊື່ນະໂຍບາຍທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['policy_manager_policy_not_found'] = 'ນະໂຍບາຍບໍ່ໄດ້ພົບເຫັນ.';
+$lang['policy_manager_priority_invalid'] = 'ບູລິມະສິດແມ່ນບໍ່ຖືກຕ້ອງ.';
