5c651263c79efc795cb9b8291deaf4647e253950
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Allusers ryhmä on erityinen ryhmä käytetään seurata kaikkia käyttäjiä järjestelmään. Luo uusi ryhmä, jos haluat määrittää käyttäjä- järjestelmään.';
+$lang['policy_manager_app_description'] = 'Policy Manager sovellus tarjoaa moottorin käyttöönottoon erityisiä sovelluskäytäntöjä tietyille ryhmille.';
+$lang['policy_manager_app_invalid'] = 'App on virheellinen.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Kuvaus on virheellinen.';
+$lang['policy_manager_group'] = 'Ryhmä';
+$lang['policy_manager_group_invalid'] = 'Ryhmä on virheellinen.';
+$lang['policy_manager_policies'] = 'säännöt';
+$lang['policy_manager_policy'] = 'politiikka';
+$lang['policy_manager_policy_already_exists'] = 'Suoja on jo olemassa.';
+$lang['policy_manager_policy_id_invalid'] = 'Suoja on virheellinen.';
+$lang['policy_manager_policy_name'] = 'Policy Name';
+$lang['policy_manager_policy_name_invalid'] = 'Käytännön nimi on virheellinen.';
+$lang['policy_manager_policy_not_found'] = 'Suoja ei löytynyt.';
+$lang['policy_manager_priority_invalid'] = 'Priority on virheellinen.';
