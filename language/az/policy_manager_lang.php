00bab8a320290a694199f25dec22bf4cdabeda86
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers qrup sistem bütün istifadəçilər izlemek üçün istifadə xüsusi qrupudur. Siz sistem istifadəçilərinin alt müəyyən etmək lazımdır, əgər yeni bir qrup yaradın.';
+$lang['policy_manager_app_description'] = 'Siyasət Manager app xüsusi qruplar üçün xüsusi app siyasəti yerləşdirilməsi üçün mühərrik təmin edir.';
+$lang['policy_manager_app_invalid'] = 'App yanlışdır.';
+$lang['policy_manager_app_name'] = 'Siyasət Manager';
+$lang['policy_manager_description_invalid'] = 'Təsvir yanlışdır.';
+$lang['policy_manager_group'] = 'qrup';
+$lang['policy_manager_group_invalid'] = 'Group yanlışdır.';
+$lang['policy_manager_policies'] = 'siyasəti';
+$lang['policy_manager_policy'] = 'siyasət';
+$lang['policy_manager_policy_already_exists'] = 'Siyasət artıq mövcuddur.';
+$lang['policy_manager_policy_id_invalid'] = 'Siyasət ID yanlışdır.';
+$lang['policy_manager_policy_name'] = 'Siyasət Adı';
+$lang['policy_manager_policy_name_invalid'] = 'Siyasət səhvdir.';
+$lang['policy_manager_policy_not_found'] = 'Siyasət tapılmadı.';
+$lang['policy_manager_priority_invalid'] = 'Priority yanlışdır.';
