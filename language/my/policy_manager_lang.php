bef40d01b2cee05e107190e33170225a89476531
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'အဆိုပါ allusers အုပ်စုကစနစ်ပေါ်မှာရှိသမျှသောအသုံးပြုသူများကိုခြေရာခံရန်အသုံးပြုတဲ့အထူးအဖွဲ့ဖြစ်တယ်။ သင် system ပေါ်တွင်အသုံးပြုသူအပိုင်းတစ်ပိုင်းကိုသာလျှင်သတ်မှတ်ဖို့လိုအပ်မယ်ဆိုရင်တစ်ဦးအုပ်စုအသစ်ဖန်တီးပါ။';
+$lang['policy_manager_app_description'] = 'အဆိုပါပေါ်လစီ Manager ကို app ကိုတိကျတဲ့အုပ်စုများမှတိကျတဲ့ app ကိုမူဝါဒများစေလွှတ်ဘို့အင်ဂျင်ပေးစွမ်းသည်။';
+$lang['policy_manager_app_invalid'] = 'App ကိုမမှန်ကန်ဘူး။';
+$lang['policy_manager_app_name'] = 'ပေါ်လစီ Manager ကို';
+$lang['policy_manager_description_invalid'] = 'ဖော်ပြချက်ဟာမမှန်ကန်ဘူး။';
+$lang['policy_manager_group'] = 'Group က';
+$lang['policy_manager_group_invalid'] = 'Group ကမမှန်ကန်ဘူး။';
+$lang['policy_manager_policies'] = 'မူဝါဒများ';
+$lang['policy_manager_policy'] = 'ပေါ်လစီ';
+$lang['policy_manager_policy_already_exists'] = 'ပေါ်လစီရှိပြီးဖြစ်သည်။';
+$lang['policy_manager_policy_id_invalid'] = 'ပေါ်လစီ ID ကိုမမှန်ကန်ဘူး။';
+$lang['policy_manager_policy_name'] = 'ပေါ်လစီအမည်';
+$lang['policy_manager_policy_name_invalid'] = 'ပေါ်လစီအမည်မမှန်ကန်ပါ။';
+$lang['policy_manager_policy_not_found'] = 'ပေါ်လစီမတွေ့ရှိ။';
+$lang['policy_manager_priority_invalid'] = 'ဦးစားပေးမမှန်ကန်ဘူး။';
