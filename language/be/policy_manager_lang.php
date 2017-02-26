c284f112c6b07a92ea75e75f312ef513f3db3e39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Група AllUsers спецыяльная група, якая выкарыстоўваецца для адсочвання ўсіх карыстальнікаў сістэмы. Калі ласка, стварыце новую групу, калі вам трэба паказаць падмноства карыстальнікаў у сістэме.';
+$lang['policy_manager_app_description'] = 'Дадатак дыспетчар палітык забяспечвае механізм для разгортвання канкрэтнай палітыкі прыкладанняў для канкрэтных груп.';
+$lang['policy_manager_app_invalid'] = 'App з&#39;яўляецца несапраўдным.';
+$lang['policy_manager_app_name'] = 'Менеджэр па палітыцы';
+$lang['policy_manager_description_invalid'] = 'Апісанне ня дзейнічае.';
+$lang['policy_manager_group'] = 'група';
+$lang['policy_manager_group_invalid'] = 'Група з&#39;яўляецца несапраўдным.';
+$lang['policy_manager_policies'] = 'полісы';
+$lang['policy_manager_policy'] = 'палітыка';
+$lang['policy_manager_policy_already_exists'] = 'Палітыка ўжо існуе.';
+$lang['policy_manager_policy_id_invalid'] = 'Палітыка ID ня дзейнічае.';
+$lang['policy_manager_policy_name'] = 'Імя палітыкі';
+$lang['policy_manager_policy_name_invalid'] = 'Імя палітыкі з&#39;яўляецца несапраўдным.';
+$lang['policy_manager_policy_not_found'] = 'Палітыка не знойдзены.';
+$lang['policy_manager_priority_invalid'] = 'Прыярытэт з&#39;яўляецца несапраўдным.';
