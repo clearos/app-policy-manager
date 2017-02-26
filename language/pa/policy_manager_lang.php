7a75f8c1a0c5ec36331449a8e402f7fd675bb8e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers ਗਰੁੱਪ ਨੂੰ ਇੱਕ ਖਾਸ ਸਿਸਟਮ ਤੇ ਸਭ ਉਪਭੋਗੀ ਨੂੰ ਟਰੈਕ ਕਰਨ ਲਈ ਵਰਤਿਆ ਗਰੁੱਪ ਨੂੰ ਹੁੰਦਾ ਹੈ. ਜੇ ਤੁਹਾਨੂੰ ਸਿਸਟਮ ਉੱਪਰ ਉਪਭੋਗੀ ਦੇ ਇੱਕ ਸਮੂਹ ਨੂੰ ਨਿਰਧਾਰਿਤ ਕਰਨ ਦੀ ਲੋੜ ਹੈ, ਕਿਰਪਾ ਕਰਕੇ ਇੱਕ ਨਵ ਗਰੁੱਪ ਬਣਾਓ.';
+$lang['policy_manager_app_description'] = 'ਨੀਤੀ ਮੈਨੇਜਰ ਐਪ ਖਾਸ ਗਰੁੱਪ ਲਈ ਹੀ ਖਾਸ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਪਾਲਸੀ ਨੂੰ ਵੰਡਣ ਦੀ ਇੰਜਣ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['policy_manager_app_invalid'] = 'ਐਪ ਗਲਤ ਹੈ.';
+$lang['policy_manager_app_name'] = 'ਨੀਤੀ ਮੈਨੇਜਰ';
+$lang['policy_manager_description_invalid'] = 'ਵੇਰਵਾ ਗਲਤ ਹੈ.';
+$lang['policy_manager_group'] = 'ਗਰੁੱਪ';
+$lang['policy_manager_group_invalid'] = 'ਗਰੁੱਪ ਗਲਤ ਹੈ.';
+$lang['policy_manager_policies'] = 'ਡਰਾਇਰ';
+$lang['policy_manager_policy'] = 'ਨੀਤੀ ਨੂੰ';
+$lang['policy_manager_policy_already_exists'] = 'ਨੀਤੀ ਹੀ ਮੌਜੂਦ ਹੈ.';
+$lang['policy_manager_policy_id_invalid'] = 'ਨੀਤੀ ID ਨੂੰ ਗਲਤ ਹੈ.';
+$lang['policy_manager_policy_name'] = 'ਨੀਤੀ ਦਾ ਨਾਮ';
+$lang['policy_manager_policy_name_invalid'] = 'ਨੀਤੀ ਦਾ ਨਾਮ ਗਲਤ ਹੈ.';
+$lang['policy_manager_policy_not_found'] = 'ਨੀਤੀ ਨਾ ਲੱਭੀ.';
+$lang['policy_manager_priority_invalid'] = 'ਤਰਜੀਹ ਗਲਤ ਹੈ.';
