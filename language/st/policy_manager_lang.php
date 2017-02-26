496d3d4464cacb76afb134dede0de5442eaa7c95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers sehlopha se ke sehlopha se khethehileng sebelisoa ho Track basebedisi tsohle ka tsamaiso ea. Ka kōpo bōpa sehlopha se le lecha haeba u lokela ho bolela ka ho toba e karolo la basebedisi tsamaiso.';
+$lang['policy_manager_app_description'] = 'The app e Manager Policy fana ka enjene bakeng deploying maano tse tobileng tseo app e ho lihlopha tse tobileng.';
+$lang['policy_manager_app_invalid'] = 'App ha o sebetse.';
+$lang['policy_manager_app_name'] = 'Manager Policy';
+$lang['policy_manager_description_invalid'] = 'Tlhaloso ha o sebetse.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group ha o sebetse.';
+$lang['policy_manager_policies'] = 'maano';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy se a ntse a teng.';
+$lang['policy_manager_policy_id_invalid'] = 'ID Policy ha o sebetse.';
+$lang['policy_manager_policy_name'] = 'Name Policy';
+$lang['policy_manager_policy_name_invalid'] = 'lebitso Policy ha o sebetse.';
+$lang['policy_manager_policy_not_found'] = 'Policy ha le a fumanwa.';
+$lang['policy_manager_priority_invalid'] = 'Etelletsa pele ha o sebetse.';
