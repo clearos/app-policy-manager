0a6d8dd961cb5d3defdf06b942b42da07ffed759
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'මෙම allusers පිරිසක් පද්ධතිය මත සියලු පරිශීලකයන්ට නිරීක්ෂණය කිරීමට භාවිතා කරන ලද විශේෂ කණ්ඩායමක් වේ. ඔබ පද්ධතිය පරිශීලකයන් උප කුලකයක් නියම කිරීමට අවශ්ය නම් නව කණ්ඩායමක් නිර්මාණය කරන්න.';
+$lang['policy_manager_app_description'] = 'මෙම ප්රතිපත්ති කළමනාකරු යෙදුම විශේෂිත කණ්ඩායම් සඳහා විශේෂිත යෙදුම ප්රතිපත්ති සඳහා යෙදවිම එන්ජිම සපයයි.';
+$lang['policy_manager_app_invalid'] = 'යෙදුම වලංගු නැත.';
+$lang['policy_manager_app_name'] = 'ප්රතිපත්ති කළමනාකරු';
+$lang['policy_manager_description_invalid'] = 'විස්තරය වලංගු නැත.';
+$lang['policy_manager_group'] = 'සමූහය';
+$lang['policy_manager_group_invalid'] = 'සමූහ වලංගු නැත.';
+$lang['policy_manager_policies'] = 'පිලිවෙත්';
+$lang['policy_manager_policy'] = 'ප්රතිපත්ති';
+$lang['policy_manager_policy_already_exists'] = 'ප්රතිපත්ති දැනටමත් පවතී.';
+$lang['policy_manager_policy_id_invalid'] = 'ප්රතිපත්ති හැඳුනුම්පත වලංගු නැත.';
+$lang['policy_manager_policy_name'] = 'ප්රතිපත්ති නම';
+$lang['policy_manager_policy_name_invalid'] = 'ප්රතිපත්ති නම වලංගු නැත.';
+$lang['policy_manager_policy_not_found'] = 'ප්රතිපත්ති දක්නට නැත.';
+$lang['policy_manager_priority_invalid'] = 'ප්රමුඛ වලංගු නැත.';
