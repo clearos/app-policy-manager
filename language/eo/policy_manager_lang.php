9d5e150475e9d0d3f57a0d0e6901933ab6548fac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'La allusers grupo estas speciala grupo uzita spuri ĉiuj uzantoj en la sistemo. Bonvolu krei novan grupon se vi bezonos specifi subaro de uzantoj en la sistemo.';
+$lang['policy_manager_app_description'] = 'La Politiko Manager app provizas la motoron por deploji specifa app politikoj por specifaj grupoj.';
+$lang['policy_manager_app_invalid'] = 'App estas nevalida.';
+$lang['policy_manager_app_name'] = 'politiko Direktisto';
+$lang['policy_manager_description_invalid'] = 'Priskribo estas nevalida.';
+$lang['policy_manager_group'] = 'grupo';
+$lang['policy_manager_group_invalid'] = 'Grupo estas nevalida.';
+$lang['policy_manager_policies'] = 'politikoj';
+$lang['policy_manager_policy'] = 'politiko';
+$lang['policy_manager_policy_already_exists'] = 'Politiko jam ekzistas.';
+$lang['policy_manager_policy_id_invalid'] = 'Politiko ID estas nevalida.';
+$lang['policy_manager_policy_name'] = 'politiko Nomo';
+$lang['policy_manager_policy_name_invalid'] = 'Politiko nomo estas malvalida.';
+$lang['policy_manager_policy_not_found'] = 'Politiko ne trovis.';
+$lang['policy_manager_priority_invalid'] = 'Prioritato estas nevalida.';
