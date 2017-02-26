d36cda99bd3a95ca54811af738a9c6af6c733678
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers गट प्रणाली सर्व वापरकर्ते ट्रॅक वापरले विशेष गट आहे. आपण प्रणाली वापरकर्त्यांची एक उपसंच निर्देशीत करणे आवश्यक आहे तर नवीन गट तयार करा.';
+$lang['policy_manager_app_description'] = 'धोरण व्यवस्थापक अनुप्रयोग विशिष्ट गट विशिष्ट अनुप्रयोग धोरणे वितरण करीता इंजिन उपलब्ध आहे.';
+$lang['policy_manager_app_invalid'] = 'अनुप्रयोग अवैध आहे.';
+$lang['policy_manager_app_name'] = 'धोरण व्यवस्थापक';
+$lang['policy_manager_description_invalid'] = 'वर्णन अवैध आहे.';
+$lang['policy_manager_group'] = 'गट';
+$lang['policy_manager_group_invalid'] = 'गट अवैध आहे.';
+$lang['policy_manager_policies'] = 'धोरणे';
+$lang['policy_manager_policy'] = 'धोरण';
+$lang['policy_manager_policy_already_exists'] = 'धोरण आधीपासून विद्यमान आहे.';
+$lang['policy_manager_policy_id_invalid'] = 'धोरण आयडी अवैध आहे.';
+$lang['policy_manager_policy_name'] = 'धोरणाचे नाव';
+$lang['policy_manager_policy_name_invalid'] = 'धोरण नाव अवैध आहे.';
+$lang['policy_manager_policy_not_found'] = 'धोरण आढळले नाही.';
+$lang['policy_manager_priority_invalid'] = 'प्राधान्य अवैध आहे.';
