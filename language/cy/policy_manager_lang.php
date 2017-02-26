e3a0cd6fdb0e388eb8a4945fcc29aa8323447748
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Mae&#39;r grŵp allusers yn grŵp arbennig a ddefnyddir i olrhain yr holl ddefnyddwyr ar y system. Os gwelwch yn dda creu grŵp newydd os oes angen i nodi is-set o ddefnyddwyr ar y system.';
+$lang['policy_manager_app_description'] = 'Y Rheolwr Polisi app yn darparu&#39;r peiriant ar gyfer defnyddio polisïau app penodol i grwpiau penodol.';
+$lang['policy_manager_app_invalid'] = 'App yn annilys.';
+$lang['policy_manager_app_name'] = 'Rheolwr polisi';
+$lang['policy_manager_description_invalid'] = 'Disgrifiad yn annilys.';
+$lang['policy_manager_group'] = 'grŵp';
+$lang['policy_manager_group_invalid'] = 'Grŵp yn annilys.';
+$lang['policy_manager_policies'] = 'Polisïau';
+$lang['policy_manager_policy'] = 'polisi';
+$lang['policy_manager_policy_already_exists'] = 'Polisi eisoes yn bodoli.';
+$lang['policy_manager_policy_id_invalid'] = 'Polisi ID yn annilys.';
+$lang['policy_manager_policy_name'] = 'polisi Enw';
+$lang['policy_manager_policy_name_invalid'] = 'enw Polisi yn annilys.';
+$lang['policy_manager_policy_not_found'] = 'Polisi heb ei darganfod.';
+$lang['policy_manager_priority_invalid'] = 'Blaenoriaeth yn annilys.';
