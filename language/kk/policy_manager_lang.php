af602630956c795e0e440a46f6cba34f5de9d87a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'көрсетілген тобының жүйедегі барлық пайдаланушыларды қадағалау үшін пайдаланылатын арнайы топ болып табылады. Сіз жүйеге пайдаланушылар жиынын белгілеу үшін қажет болса, жаңа топты жасау сұраймыз.';
+$lang['policy_manager_app_description'] = 'Саясаты менеджері Қолданба нақты топтарына нақты бағдарлама саясатын қолдану үшін қозғалтқыш қамтамасыз етеді.';
+$lang['policy_manager_app_invalid'] = 'App жарамсыз болып табылады.';
+$lang['policy_manager_app_name'] = 'саясаты менеджері';
+$lang['policy_manager_description_invalid'] = 'Сипаттамасы жарамсыз болып табылады.';
+$lang['policy_manager_group'] = 'топ';
+$lang['policy_manager_group_invalid'] = 'Group жарамсыз болып табылады.';
+$lang['policy_manager_policies'] = 'Саясат';
+$lang['policy_manager_policy'] = 'саясат';
+$lang['policy_manager_policy_already_exists'] = 'Саясаты бұрыннан бар.';
+$lang['policy_manager_policy_id_invalid'] = 'Саясаты ID жарамсыз болып табылады.';
+$lang['policy_manager_policy_name'] = 'саясаты атауы';
+$lang['policy_manager_policy_name_invalid'] = 'Саясаты аты жарамсыз.';
+$lang['policy_manager_policy_not_found'] = 'Саясаты табылған жоқ.';
+$lang['policy_manager_priority_invalid'] = 'Басымдылық жарамсыз болып табылады.';
