7a8854ad7ca56276607c5de3cbd347bb23870a83
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers gulu gulu lapadera ntchito younikira owerenga yonse dongosolo. Chonde kulenga gulu latsopano ngati muyenera mwatchutchutchu kagawo wa owerenga pa dongosolo.';
+$lang['policy_manager_app_description'] = 'The app Manager Policy imapereka injini kwa deploying mfundo zenizeni app kuti magulu akutiakuti.';
+$lang['policy_manager_app_invalid'] = 'App ndi opanda pake.';
+$lang['policy_manager_app_name'] = 'Manager Policy';
+$lang['policy_manager_description_invalid'] = 'Kufotokozera ndi opanda pake.';
+$lang['policy_manager_group'] = 'gulu';
+$lang['policy_manager_group_invalid'] = 'Gulu ili udzakhala osazindikilika.';
+$lang['policy_manager_policies'] = 'ndondomeko';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy lilipo kale.';
+$lang['policy_manager_policy_id_invalid'] = 'ID Policy ndi opanda pake.';
+$lang['policy_manager_policy_name'] = 'Name Policy';
+$lang['policy_manager_policy_name_invalid'] = 'dzina Policy ndi opanda pake.';
+$lang['policy_manager_policy_not_found'] = 'Policy sanapezeke.';
+$lang['policy_manager_priority_invalid'] = 'Patsogolo ndi opanda pake.';
