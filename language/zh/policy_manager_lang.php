ab62514336de745339db28457ee9c6711854980b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'AllUsers群组是用于跟踪所有系统上的用户的一个特殊群体。 如果您需要在系统上指定的用户的一个子集，请创建一个新组。';
+$lang['policy_manager_app_description'] = '策略管理器应用程序提供了特定人群部署特定应用的政策引擎。';
+$lang['policy_manager_app_invalid'] = '应用程序是无效的。';
+$lang['policy_manager_app_name'] = '策略管理器';
+$lang['policy_manager_description_invalid'] = '说明是无效的。';
+$lang['policy_manager_group'] = '组';
+$lang['policy_manager_group_invalid'] = '集团是无效的。';
+$lang['policy_manager_policies'] = '政策';
+$lang['policy_manager_policy'] = '政策';
+$lang['policy_manager_policy_already_exists'] = '政策已经存在。';
+$lang['policy_manager_policy_id_invalid'] = '政策ID无效。';
+$lang['policy_manager_policy_name'] = '策略名称';
+$lang['policy_manager_policy_name_invalid'] = '策略名称是无效的。';
+$lang['policy_manager_policy_not_found'] = '政策未找到。';
+$lang['policy_manager_priority_invalid'] = '优先级无效。';
