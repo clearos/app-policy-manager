4b52facdd5b2e43da7ec27723d306d7c8f1bb60d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'group allusers waa koox gaar ah oo loo isticmaalo si ay ula socdaan oo dhan users on nidaamka. Fadlan la abuuro koox cusub haddii aad u baahan tahay in la qeexo hoosaad ka mid ah dadka isticmaala on nidaamka.';
+$lang['policy_manager_app_description'] = 'app Manager Policy wuxuu bixiyaa engine ee ciidamo siyaasado gaar app in kooxo gaar ah.';
+$lang['policy_manager_app_invalid'] = 'App waa baadil.';
+$lang['policy_manager_app_name'] = 'Maareeyaha Policy';
+$lang['policy_manager_description_invalid'] = 'Description waa baadil.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group waa baadil.';
+$lang['policy_manager_policies'] = 'Siyaasadaha';
+$lang['policy_manager_policy'] = 'Siyaasadda';
+$lang['policy_manager_policy_already_exists'] = 'Siyaasadda hore u jirta.';
+$lang['policy_manager_policy_id_invalid'] = 'ID Policy waa baadil.';
+$lang['policy_manager_policy_name'] = 'Siyaasadda Name';
+$lang['policy_manager_policy_name_invalid'] = 'Magaca Policy waa baadil.';
+$lang['policy_manager_policy_not_found'] = 'Siyaasadda ma ay helin.';
+$lang['policy_manager_priority_invalid'] = 'Ahmiyada waa baadil.';
