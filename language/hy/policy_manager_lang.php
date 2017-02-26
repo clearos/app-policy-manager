bcf9cd121aa0df76ecf6c696143258791624a26a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:28:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers խումբը հատուկ խումբ, որն օգտագործվում է հետեւելու բոլոր օգտվողներին համակարգի. Խնդրում ենք ստեղծել նոր խումբ, եթե դուք պետք է նշեք մի ենթաբազմություն օգտագործողների համակարգի:';
+$lang['policy_manager_app_description'] = 'Քաղաքականության տնօրեն ծրագիրը ապահովում է շարժիչը տեղակայման հատուկ հավելվածների քաղաքականությունը կոնկրետ խմբերի.';
+$lang['policy_manager_app_invalid'] = 'App է անվավեր:';
+$lang['policy_manager_app_name'] = 'քաղաքականություն մենեջեր';
+$lang['policy_manager_description_invalid'] = 'Նկարագրություն անվավեր է:';
+$lang['policy_manager_group'] = 'խումբ';
+$lang['policy_manager_group_invalid'] = 'Group անվավեր է:';
+$lang['policy_manager_policies'] = 'քաղաքականությունը';
+$lang['policy_manager_policy'] = 'քաղաքականություն';
+$lang['policy_manager_policy_already_exists'] = 'Քաղաքականություն արդեն գոյություն ունի.';
+$lang['policy_manager_policy_id_invalid'] = 'Քաղաքականություն ID- ն անվավեր է:';
+$lang['policy_manager_policy_name'] = 'քաղաքականություն Անունը';
+$lang['policy_manager_policy_name_invalid'] = 'Քաղաքականություն անունը անվավեր է:';
+$lang['policy_manager_policy_not_found'] = 'Քաղաքականություն չի գտնվել:';
+$lang['policy_manager_priority_invalid'] = 'Priority է անվավեր:';
