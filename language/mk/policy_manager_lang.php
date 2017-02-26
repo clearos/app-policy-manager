5cc2bb36c0ee8df5c499b8b26b1e6d6c2cc5147d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'група allusers е посебна група се користи за следење на сите корисници на системот. Ве молиме креирате нова група, ако треба да се определи една подгрупа на корисници на системот.';
+$lang['policy_manager_app_description'] = 'Апликацијата на менаџер политика им нуди на моторот за имплементирање на специфични политики за апликација на одредени групи.';
+$lang['policy_manager_app_invalid'] = 'Стан не е валиден.';
+$lang['policy_manager_app_name'] = 'менаџер политика';
+$lang['policy_manager_description_invalid'] = 'Опис на играта не е валиден.';
+$lang['policy_manager_group'] = 'група';
+$lang['policy_manager_group_invalid'] = 'Групата не е валиден.';
+$lang['policy_manager_policies'] = 'политики';
+$lang['policy_manager_policy'] = 'политика';
+$lang['policy_manager_policy_already_exists'] = 'Политика веќе постои.';
+$lang['policy_manager_policy_id_invalid'] = 'Политика проект не е валиден.';
+$lang['policy_manager_policy_name'] = 'политика Име';
+$lang['policy_manager_policy_name_invalid'] = 'име политика не е валиден.';
+$lang['policy_manager_policy_not_found'] = 'Политика не е пронајден.';
+$lang['policy_manager_priority_invalid'] = 'Приоритет е валиден.';
