09d781d2ef484fc77789d79be2f818bf38415819
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'El grup allusers és un grup especial que s&#39;utilitza per realitzar un seguiment de tots els usuaris del sistema. Si us plau, creeu un nou grup si cal especificar un subconjunt d&#39;usuaris en el sistema.';
+$lang['policy_manager_app_description'] = 'L&#39;aplicació de política Manager proporciona el motor per a la implementació de polítiques específiques de l&#39;aplicació per a grups específics.';
+$lang['policy_manager_app_invalid'] = 'App no ​​és vàlid.';
+$lang['policy_manager_app_name'] = 'Administrador de directives';
+$lang['policy_manager_description_invalid'] = 'Descripció no és vàlid.';
+$lang['policy_manager_group'] = 'grup';
+$lang['policy_manager_group_invalid'] = 'Grup no és vàlid.';
+$lang['policy_manager_policies'] = 'polítiques';
+$lang['policy_manager_policy'] = 'política';
+$lang['policy_manager_policy_already_exists'] = 'Política ja existeix.';
+$lang['policy_manager_policy_id_invalid'] = 'ID de política no és vàlid.';
+$lang['policy_manager_policy_name'] = 'Nom de la política';
+$lang['policy_manager_policy_name_invalid'] = 'Nom de la política no és vàlid.';
+$lang['policy_manager_policy_not_found'] = 'La política no es troba.';
+$lang['policy_manager_priority_invalid'] = 'Prioritat no és vàlid.';
