1a189e723b647b6ebcd4d76f69e1cd023c569837
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'กลุ่ม ALLUSERS เป็นกลุ่มพิเศษที่ใช้ในการติดตามผู้ใช้ทั้งหมดในระบบ โปรดสร้างกลุ่มใหม่ถ้าคุณจำเป็นต้องระบุกลุ่มย่อยของผู้ใช้ในระบบ';
+$lang['policy_manager_app_description'] = 'แอปพลิเคนโยบายจัดการให้เครื่องยนต์สำหรับการปรับใช้แอพพลิเคนโยบายที่เฉพาะเจาะจงกับกลุ่มที่เฉพาะเจาะจง';
+$lang['policy_manager_app_invalid'] = 'App ไม่ถูกต้อง';
+$lang['policy_manager_app_name'] = 'ผู้จัดการฝ่ายนโยบาย';
+$lang['policy_manager_description_invalid'] = 'คำอธิบายไม่ถูกต้อง';
+$lang['policy_manager_group'] = 'กลุ่ม';
+$lang['policy_manager_group_invalid'] = 'กลุ่มที่ไม่ถูกต้อง';
+$lang['policy_manager_policies'] = 'นโยบาย';
+$lang['policy_manager_policy'] = 'นโยบาย';
+$lang['policy_manager_policy_already_exists'] = 'นโยบายที่มีอยู่แล้ว';
+$lang['policy_manager_policy_id_invalid'] = 'นโยบายรหัสไม่ถูกต้อง';
+$lang['policy_manager_policy_name'] = 'ชื่อนโยบาย';
+$lang['policy_manager_policy_name_invalid'] = 'ชื่อนโยบายไม่ถูกต้อง';
+$lang['policy_manager_policy_not_found'] = 'ไม่พบนโยบาย';
+$lang['policy_manager_priority_invalid'] = 'ลำดับความสำคัญไม่ถูกต้อง';
