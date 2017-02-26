72bfe081da018f8bba404ccdb09cf97f7e13bee7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'A allusers csoport egy speciális csoport használják a pálya minden felhasználó számára a rendszer. Kérjük, hozzon létre egy új csoportot, ha meg kell adnia a felhasználók egy része a rendszer.';
+$lang['policy_manager_app_description'] = 'A Policy Manager alkalmazás biztosítja a motor telepítésével bizonyos alkalmazás politika bizonyos csoportokat.';
+$lang['policy_manager_app_invalid'] = 'App érvénytelen.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Leírás érvénytelen.';
+$lang['policy_manager_group'] = 'Csoport';
+$lang['policy_manager_group_invalid'] = 'Csoport érvénytelen.';
+$lang['policy_manager_policies'] = 'házirendje';
+$lang['policy_manager_policy'] = 'Irányelv';
+$lang['policy_manager_policy_already_exists'] = 'Politika már létezik.';
+$lang['policy_manager_policy_id_invalid'] = 'Policy azonosító érvénytelen.';
+$lang['policy_manager_policy_name'] = 'A szabály neve';
+$lang['policy_manager_policy_name_invalid'] = 'Politika érvénytelen.';
+$lang['policy_manager_policy_not_found'] = 'Politika nem található.';
+$lang['policy_manager_priority_invalid'] = 'Prioritás érvénytelen.';
