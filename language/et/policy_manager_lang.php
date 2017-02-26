97677e0bfd45569c034bc6f818e128b3ace61494
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Allusers grupp on erirühma kasutatakse, et jälgida kõiki süsteemi kasutajatele. Palun Uue grupi kui teil on vaja täpsustada alagrupis süsteemi kasutajatele.';
+$lang['policy_manager_app_description'] = 'Poliitika Manager app annab mootori kutsumise konkreetse rakenduse reegleid konkreetsete rühmadega.';
+$lang['policy_manager_app_invalid'] = 'App on kehtetu.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Kirjeldus ei sobi.';
+$lang['policy_manager_group'] = 'Grupp';
+$lang['policy_manager_group_invalid'] = 'Group on kehtetu.';
+$lang['policy_manager_policies'] = 'poliitika';
+$lang['policy_manager_policy'] = 'poliitika';
+$lang['policy_manager_policy_already_exists'] = 'Poliitika on juba olemas.';
+$lang['policy_manager_policy_id_invalid'] = 'Poliitika ID ei sobi.';
+$lang['policy_manager_policy_name'] = 'poliitika Nimi';
+$lang['policy_manager_policy_name_invalid'] = 'Poliitika nimi on kehtetu.';
+$lang['policy_manager_policy_not_found'] = 'Poliitika ei leitud.';
+$lang['policy_manager_priority_invalid'] = 'Prioriteet on kehtetu.';
