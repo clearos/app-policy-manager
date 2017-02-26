e62b43706ed33dbcb0078a52847a024f4c138b6d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers kundi ni kundi maalum kutumika kufuatilia watumiaji wote juu ya mfumo. Tafadhali kuunda kikundi kipya kama wewe haja ya kutaja subset ya watumiaji juu ya mfumo.';
+$lang['policy_manager_app_description'] = 'programu Meneja Sera hutoa injini kwa ajili ya kupeleka sera maalum programu kwa makundi maalum.';
+$lang['policy_manager_app_invalid'] = 'Programu ni batili.';
+$lang['policy_manager_app_name'] = 'Meneja Sera';
+$lang['policy_manager_description_invalid'] = 'Maelezo ni batili.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group ni batili.';
+$lang['policy_manager_policies'] = 'sera';
+$lang['policy_manager_policy'] = 'Sera';
+$lang['policy_manager_policy_already_exists'] = 'Sera tayari ipo.';
+$lang['policy_manager_policy_id_invalid'] = 'ID sera ni batili.';
+$lang['policy_manager_policy_name'] = 'Jina Sera';
+$lang['policy_manager_policy_name_invalid'] = 'jina sera ni batili.';
+$lang['policy_manager_policy_not_found'] = 'Sera haikupatikana.';
+$lang['policy_manager_priority_invalid'] = 'Kipaumbele ni batili.';
