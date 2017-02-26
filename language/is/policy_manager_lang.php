1c5ecc6795d6a84a352ba12813ef7dab99230159
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers hópur er sérstakur hópur sem er notað til að fylgjast með öllum notendum á kerfinu. Vinsamlegast búa til nýjan hóp ef þú þarft að tilgreina hlutmengi notendur á kerfinu.';
+$lang['policy_manager_app_description'] = 'Stefnan Manager app veitir vél fyrir beita sértækar reglur app til sérstakra hópa.';
+$lang['policy_manager_app_invalid'] = 'App er ógilt.';
+$lang['policy_manager_app_name'] = 'stefna Manager';
+$lang['policy_manager_description_invalid'] = 'Lýsing er ógilt.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group er ógilt.';
+$lang['policy_manager_policies'] = 'Reglur';
+$lang['policy_manager_policy'] = 'stefna';
+$lang['policy_manager_policy_already_exists'] = 'Stefna er þegar til.';
+$lang['policy_manager_policy_id_invalid'] = 'Stefna ID er ógilt.';
+$lang['policy_manager_policy_name'] = 'stefna Name';
+$lang['policy_manager_policy_name_invalid'] = 'Stefna er ógilt.';
+$lang['policy_manager_policy_not_found'] = 'Stefna fannst ekki.';
+$lang['policy_manager_priority_invalid'] = 'Forgangur er ógilt.';
