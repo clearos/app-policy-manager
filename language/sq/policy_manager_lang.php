bb340cf35b807f5cdb726c16dc2aadc09b34c134
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:28:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Grupi allusers është një grup i posaçëm përdorur për të ndjekur të gjithë përdoruesit në sistemin. Ju lutem të krijuar një grup të ri, nëse ju duhet të specifikoni një mesin e përdoruesve në sistemin.';
+$lang['policy_manager_app_description'] = 'App Politika Menaxheri siguron motori për vendosjen e politikave specifike app për grupe të veçanta.';
+$lang['policy_manager_app_invalid'] = 'App është i pavlefshëm.';
+$lang['policy_manager_app_name'] = 'Politika Menaxher';
+$lang['policy_manager_description_invalid'] = 'Përshkrimi është i pavlefshëm.';
+$lang['policy_manager_group'] = 'grup';
+$lang['policy_manager_group_invalid'] = 'Grupi është i pavlefshëm.';
+$lang['policy_manager_policies'] = 'politikat';
+$lang['policy_manager_policy'] = 'politikë';
+$lang['policy_manager_policy_already_exists'] = 'Politika tashmë ekziston.';
+$lang['policy_manager_policy_id_invalid'] = 'ID politika është e pavlefshme.';
+$lang['policy_manager_policy_name'] = 'Politika Emri';
+$lang['policy_manager_policy_name_invalid'] = 'Emri Politika është i pavlefshëm.';
+$lang['policy_manager_policy_not_found'] = 'Politika nuk u gjet.';
+$lang['policy_manager_priority_invalid'] = 'Prioritet është e pavlefshme.';
