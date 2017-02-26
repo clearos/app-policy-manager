904764e298b2aeeea7a4cce8319a4b01e8a3f6f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:30:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Den ALLUSERS gruppe er en særlig gruppe bruges til at spore alle brugere på systemet. Opret en ny gruppe, hvis du har brug for at angive en delmængde af brugere på systemet.';
+$lang['policy_manager_app_description'] = 'Den Policy Manager app giver motoren til implementering specifikke app politikker til specifikke grupper.';
+$lang['policy_manager_app_invalid'] = 'App er ugyldig.';
+$lang['policy_manager_app_name'] = 'Policy manager';
+$lang['policy_manager_description_invalid'] = 'Beskrivelse er ugyldig.';
+$lang['policy_manager_group'] = 'Gruppe';
+$lang['policy_manager_group_invalid'] = 'Group er ugyldig.';
+$lang['policy_manager_policies'] = 'Politikker';
+$lang['policy_manager_policy'] = 'Politik';
+$lang['policy_manager_policy_already_exists'] = 'Politik findes allerede.';
+$lang['policy_manager_policy_id_invalid'] = 'Politik id er ugyldigt.';
+$lang['policy_manager_policy_name'] = 'Politik Navn';
+$lang['policy_manager_policy_name_invalid'] = 'Politik er ugyldigt.';
+$lang['policy_manager_policy_not_found'] = 'Politik ikke fundet.';
+$lang['policy_manager_priority_invalid'] = 'Prioritet er ugyldig.';
