72041ca335e2270cbbd82da725ccc9d474e34eb9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers જૂથ એક ખાસ સિસ્ટમ પર બધા વપરાશકર્તાઓ ટ્રૅક કરવા માટે વપરાય જૂથ છે. જો તમે સિસ્ટમ પર વપરાશકર્તાઓ ઉપગણ સ્પષ્ટ કરવાની જરૂર છે કે કૃપા કરીને એક નવું જૂથ બનાવો.';
+$lang['policy_manager_app_description'] = 'નીતિ વ્યવસ્થાપક એપ્લિકેશન ચોક્કસ જૂથો માટે ચોક્કસ એપ્લિકેશન નીતિઓ જમાવવા માટે એન્જિન પૂરી પાડે છે.';
+$lang['policy_manager_app_invalid'] = 'એપ્લિકેશન અમાન્ય છે.';
+$lang['policy_manager_app_name'] = 'નીતિ વ્યવસ્થાપક';
+$lang['policy_manager_description_invalid'] = 'વર્ણન અમાન્ય છે.';
+$lang['policy_manager_group'] = 'ગ્રુપ';
+$lang['policy_manager_group_invalid'] = 'ગ્રુપ અમાન્ય છે.';
+$lang['policy_manager_policies'] = 'નીતિઓ';
+$lang['policy_manager_policy'] = 'નીતિ';
+$lang['policy_manager_policy_already_exists'] = 'નીતિ પહેલેથી હાજર છે.';
+$lang['policy_manager_policy_id_invalid'] = 'નીતિ ID અમાન્ય છે.';
+$lang['policy_manager_policy_name'] = 'નીતિનું નામ';
+$lang['policy_manager_policy_name_invalid'] = 'નીતિનું નામ અમાન્ય છે.';
+$lang['policy_manager_policy_not_found'] = 'નીતિ મળી નથી.';
+$lang['policy_manager_priority_invalid'] = 'પ્રાધાન્યતા અમાન્ય છે.';
