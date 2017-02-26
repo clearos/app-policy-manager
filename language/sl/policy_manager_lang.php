ae3c8f8acc0e54a3d17b8f15aea62bd735425233
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:37:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Skupina ALLUSERS je posebna skupina se uporablja, da bi spremljali vse uporabnike na sistemu. Ustvarite novo skupino, če želite določiti podskupino uporabnikov na sistemu.';
+$lang['policy_manager_app_description'] = 'Aplikacija Policy Manager zagotavlja motor za uvajanje posebne politike aplikacij za posebne skupine.';
+$lang['policy_manager_app_invalid'] = 'App je neveljavna.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Opis je neveljavna.';
+$lang['policy_manager_group'] = 'skupina';
+$lang['policy_manager_group_invalid'] = 'Skupina je neveljavna.';
+$lang['policy_manager_policies'] = 'Pravila';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika že obstaja.';
+$lang['policy_manager_policy_id_invalid'] = 'ID politika je neveljavna.';
+$lang['policy_manager_policy_name'] = 'Ime politika';
+$lang['policy_manager_policy_name_invalid'] = 'Ime politika je neveljavna.';
+$lang['policy_manager_policy_not_found'] = 'Politika ni bilo mogoče najti.';
+$lang['policy_manager_priority_invalid'] = 'Prednost je neveljavna.';
