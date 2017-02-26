ea9570f24ed7f67733be32df49b5fa2c3830f426
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Die allusers Gruppe ist eine spezielle Gruppe verwendet, um alle Benutzer auf dem System zu verfolgen. Bitte legen Sie eine neue Gruppe, wenn Sie einen Teil der Benutzer auf dem System angeben müssen.';
+$lang['policy_manager_app_description'] = 'Der Policy Manager App bietet den Motor für bestimmte App Richtlinien für bestimmte Gruppen bereitstellen.';
+$lang['policy_manager_app_invalid'] = 'App ist ungültig.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Beschreibung ist ungültig.';
+$lang['policy_manager_group'] = 'Gruppe';
+$lang['policy_manager_group_invalid'] = 'Gruppe ist ungültig.';
+$lang['policy_manager_policies'] = 'Richtlinien';
+$lang['policy_manager_policy'] = 'Politik';
+$lang['policy_manager_policy_already_exists'] = 'Politik ist bereits vorhanden.';
+$lang['policy_manager_policy_id_invalid'] = 'Richtlinien-ID ist ungültig.';
+$lang['policy_manager_policy_name'] = 'Versicherungsname';
+$lang['policy_manager_policy_name_invalid'] = 'Richtlinienname ist ungültig.';
+$lang['policy_manager_policy_not_found'] = 'Politik nicht gefunden.';
+$lang['policy_manager_priority_invalid'] = 'Priorität ist ungültig.';
