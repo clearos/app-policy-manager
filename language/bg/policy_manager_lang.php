638c0b202dd4cbb4ceca9e27086662ba1a6d871f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Групата ALLUSERS е специална група, използвана за проследяване на всички потребители на системата. Моля да създадете нова група, ако трябва да се уточни подмножество на потребителите на системата.';
+$lang['policy_manager_app_description'] = 'Приложението на мениджъра политика осигурява на двигателя за разполагане на конкретни политики за приложения към специфични групи.';
+$lang['policy_manager_app_invalid'] = 'App е невалиден.';
+$lang['policy_manager_app_name'] = 'мениджър политика';
+$lang['policy_manager_description_invalid'] = 'Описание е невалиден.';
+$lang['policy_manager_group'] = 'група';
+$lang['policy_manager_group_invalid'] = 'Група е невалиден.';
+$lang['policy_manager_policies'] = 'политики';
+$lang['policy_manager_policy'] = 'политика';
+$lang['policy_manager_policy_already_exists'] = 'Политика вече съществува.';
+$lang['policy_manager_policy_id_invalid'] = 'Политика ID е невалиден.';
+$lang['policy_manager_policy_name'] = 'политика Име';
+$lang['policy_manager_policy_name_invalid'] = 'име политика е невалиден.';
+$lang['policy_manager_policy_not_found'] = 'Политика не е намерен.';
+$lang['policy_manager_priority_invalid'] = 'Приоритет е невалиден.';
