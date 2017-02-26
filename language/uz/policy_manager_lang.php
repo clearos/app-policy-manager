978148bb4f60e04b966f7eb4d5cc0cd485fdf730
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers guruh tizimi barcha foydalanuvchilarni kuzatish uchun ishlatiladigan maxsus guruh hisoblanadi. Agar tizimiga foydalanuvchilarning bir kichik majmuini belgilash kerak bo&#39;lsa, yangi guruh yaratish, iltimos.';
+$lang['policy_manager_app_description'] = 'Siyosat menejeri ilova muayyan guruhlarga xos ilova siyosatini tarqatish motorini beradi.';
+$lang['policy_manager_app_invalid'] = 'App haqiqiy emas.';
+$lang['policy_manager_app_name'] = 'siyosat menejeri';
+$lang['policy_manager_description_invalid'] = 'Tavsif haqiqiy emas.';
+$lang['policy_manager_group'] = 'guruh';
+$lang['policy_manager_group_invalid'] = 'Group haqiqiy emas.';
+$lang['policy_manager_policies'] = 'siyosat';
+$lang['policy_manager_policy'] = 'siyosat';
+$lang['policy_manager_policy_already_exists'] = 'Siyosat allaqachon mavjud.';
+$lang['policy_manager_policy_id_invalid'] = 'Siyosat ID haqiqiy emas.';
+$lang['policy_manager_policy_name'] = 'siyosat nomi';
+$lang['policy_manager_policy_name_invalid'] = 'Siyosat nomi yaroqsiz.';
+$lang['policy_manager_policy_not_found'] = 'Siyosat topilmadi.';
+$lang['policy_manager_priority_invalid'] = 'Ustuvor haqiqiy emas.';
