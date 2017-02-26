fac8068de053b7bf481f54149c3de3812f990fbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Аллусерс група је посебна група се користи за праћење све кориснике на систему. Молимо Вас да креирате нову групу ако треба да наведете подскуп корисника на систему.';
+$lang['policy_manager_app_description'] = 'Апликација политика Манагер омогућава мотор за примену посебне политике апликација за специфичне групе.';
+$lang['policy_manager_app_invalid'] = 'Апликација је неважећи.';
+$lang['policy_manager_app_name'] = 'politika директор';
+$lang['policy_manager_description_invalid'] = 'Опис сајта је неисправан.';
+$lang['policy_manager_group'] = 'Група';
+$lang['policy_manager_group_invalid'] = 'Група је неважећи.';
+$lang['policy_manager_policies'] = 'politike';
+$lang['policy_manager_policy'] = 'политика';
+$lang['policy_manager_policy_already_exists'] = 'Политика већ постоји.';
+$lang['policy_manager_policy_id_invalid'] = 'Политика ИД је неважећи.';
+$lang['policy_manager_policy_name'] = 'politika Име';
+$lang['policy_manager_policy_name_invalid'] = 'Име политика је неважећи.';
+$lang['policy_manager_policy_not_found'] = 'Политика није пронађен.';
+$lang['policy_manager_priority_invalid'] = 'Приоритет је неважећи.';
