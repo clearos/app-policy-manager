2f5edd91a14a9ef4c5b45f0cf51437a044a11f99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'यो ALLUSERS समूह प्रणालीमा सबै प्रयोगकर्ताहरू ट्रयाक गर्न प्रयोग विशेष समूह छ। तपाईं सिस्टम प्रयोगकर्ताहरूको उपसमूह निर्दिष्ट गर्न आवश्यक भएमा, कृपया एउटा नयाँ समूह सिर्जना गर्नुहोस्।';
+$lang['policy_manager_app_description'] = 'नीति प्रबन्धक अनुप्रयोग विशिष्ट समूहमा विशिष्ट अनुप्रयोग नीतिहरू परिनियोजन लागि इन्जिन प्रदान गर्दछ।';
+$lang['policy_manager_app_invalid'] = 'अनुप्रयोग अमान्य छ।';
+$lang['policy_manager_app_name'] = 'नीति प्रबन्धक';
+$lang['policy_manager_description_invalid'] = 'विवरण अमान्य छ।';
+$lang['policy_manager_group'] = 'समूह';
+$lang['policy_manager_group_invalid'] = 'समूह अवैध छ।';
+$lang['policy_manager_policies'] = 'नीतिहरू';
+$lang['policy_manager_policy'] = 'नीति';
+$lang['policy_manager_policy_already_exists'] = 'नीति पहिले नै अवस्थित छ।';
+$lang['policy_manager_policy_id_invalid'] = 'नीति आईडी अमान्य छ।';
+$lang['policy_manager_policy_name'] = 'नीति नाम';
+$lang['policy_manager_policy_name_invalid'] = 'नीति नाम अवैध छ।';
+$lang['policy_manager_policy_not_found'] = 'नीति फेला परेन।';
+$lang['policy_manager_priority_invalid'] = 'प्राथमिकता अमान्य छ।';
