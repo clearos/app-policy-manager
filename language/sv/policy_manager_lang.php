0ec6feab494d06f05912ebaf29df900bdd7fab39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Den allusers gruppen är en speciell grupp som används för att spåra alla användare i systemet. Skapa en ny grupp om du vill ange en delmängd av användare i systemet.';
+$lang['policy_manager_app_description'] = 'Policy Manager app ger motorn för distribution specifika appar politik till särskilda grupper.';
+$lang['policy_manager_app_invalid'] = 'App är ogiltig.';
+$lang['policy_manager_app_name'] = 'policy manager';
+$lang['policy_manager_description_invalid'] = 'Beskrivning är ogiltig.';
+$lang['policy_manager_group'] = 'Grupp';
+$lang['policy_manager_group_invalid'] = 'Gruppen är ogiltig.';
+$lang['policy_manager_policies'] = 'policies';
+$lang['policy_manager_policy'] = 'Politik';
+$lang['policy_manager_policy_already_exists'] = 'Policy finns redan.';
+$lang['policy_manager_policy_id_invalid'] = 'Policy-ID är ogiltigt.';
+$lang['policy_manager_policy_name'] = 'policy Name';
+$lang['policy_manager_policy_name_invalid'] = 'Policy namn är ogiltigt.';
+$lang['policy_manager_policy_not_found'] = 'Politik inte hittas.';
+$lang['policy_manager_priority_invalid'] = 'Prioritet är ogiltig.';
