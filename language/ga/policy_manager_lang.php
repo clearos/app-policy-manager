a75f689b50054cac65c26e3f13e7a7f4139af424
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Tá an grúpa allusers grúpa speisialta a úsáidtear chun rianú na húsáideoirí ar an gcóras. Tabhair chruthú grúpa nua más gá duit a shonrú fo-thacar na n-úsáideoirí ar an gcóras.';
+$lang['policy_manager_app_description'] = 'Soláthraíonn an app Bainisteoir Beartais an t-inneall le haghaidh polasaithe app ar leith imscaradh do ghrúpaí ar leith.';
+$lang['policy_manager_app_invalid'] = 'Is App neamhbhailí.';
+$lang['policy_manager_app_name'] = 'Bainisteoir Beartais';
+$lang['policy_manager_description_invalid'] = 'Is Tuairisc neamhbhailí.';
+$lang['policy_manager_group'] = 'Grúpa';
+$lang['policy_manager_group_invalid'] = 'Is grúpa neamhbhailí.';
+$lang['policy_manager_policies'] = 'Polasaithe';
+$lang['policy_manager_policy'] = 'Polasaí';
+$lang['policy_manager_policy_already_exists'] = 'Polasaí ann cheana.';
+$lang['policy_manager_policy_id_invalid'] = 'Is é polasaí ID neamhbhailí.';
+$lang['policy_manager_policy_name'] = 'Polasaí Ainm';
+$lang['policy_manager_policy_name_invalid'] = 'ainm Polasaí neamhbhailí.';
+$lang['policy_manager_policy_not_found'] = 'Polasaí gan aimsiú.';
+$lang['policy_manager_priority_invalid'] = 'Is tosaíocht neamhbhailí.';
