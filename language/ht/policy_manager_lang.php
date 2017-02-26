671b0650246b659447d80be4df464dfb9ee7ad00
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Gwoup la allusers se yon gwoup espesyal itilize yo swiv tout itilizatè yo sou sistèm nan. Tanpri kreye yon nouvo gwoup si ou bezwen presize yon sou nan itilizatè sou sistèm nan.';
+$lang['policy_manager_app_description'] = 'App a Manadjè Policy bay motè a pou deplwaye politik app espesifik nan gwoup espesifik.';
+$lang['policy_manager_app_invalid'] = 'App se valab.';
+$lang['policy_manager_app_name'] = 'Manadjè Policy';
+$lang['policy_manager_description_invalid'] = 'Deskripsyon se valab.';
+$lang['policy_manager_group'] = 'Gwoup';
+$lang['policy_manager_group_invalid'] = 'Group se valab.';
+$lang['policy_manager_policies'] = 'règleman';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy deja egziste.';
+$lang['policy_manager_policy_id_invalid'] = 'ID Policy se valab.';
+$lang['policy_manager_policy_name'] = 'Non Policy';
+$lang['policy_manager_policy_name_invalid'] = 'Non Policy se valab.';
+$lang['policy_manager_policy_not_found'] = 'Policy pa jwenn.';
+$lang['policy_manager_priority_invalid'] = 'Priyorite se valab.';
