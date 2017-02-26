b4858e7d3871b18f9f3e93a03db0b6b059ed605b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Группа AllUsers специальная группа, используемая для отслеживания всех пользователей системы. Пожалуйста, создайте новую группу, если вам нужно указать подмножество пользователей в системе.';
+$lang['policy_manager_app_description'] = 'Приложение диспетчер политик обеспечивает механизм для развертывания конкретной политики приложений для конкретных групп.';
+$lang['policy_manager_app_invalid'] = 'App является недействительным.';
+$lang['policy_manager_app_name'] = 'Менеджер по политике';
+$lang['policy_manager_description_invalid'] = 'Описание недействителен.';
+$lang['policy_manager_group'] = 'группа';
+$lang['policy_manager_group_invalid'] = 'Группа является недействительным.';
+$lang['policy_manager_policies'] = 'полисы';
+$lang['policy_manager_policy'] = 'политика';
+$lang['policy_manager_policy_already_exists'] = 'Политика уже существует.';
+$lang['policy_manager_policy_id_invalid'] = 'Политика ID недействителен.';
+$lang['policy_manager_policy_name'] = 'Имя политики';
+$lang['policy_manager_policy_name_invalid'] = 'Имя политики является недействительным.';
+$lang['policy_manager_policy_not_found'] = 'Политика не найден.';
+$lang['policy_manager_priority_invalid'] = 'Приоритет является недействительным.';
