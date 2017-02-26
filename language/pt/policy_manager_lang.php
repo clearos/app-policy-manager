76cae5c14e4b9eeba8e39cb7e6fc7200540c1447
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'O grupo allusers é um grupo especial usado para controlar todos os usuários no sistema. Por favor, criar um novo grupo, se você precisa especificar um subconjunto de usuários no sistema.';
+$lang['policy_manager_app_description'] = 'O aplicativo Policy Manager fornece o motor para a implantação de políticas de aplicativos específicos para grupos específicos.';
+$lang['policy_manager_app_invalid'] = 'App é inválido.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Descrição é inválido.';
+$lang['policy_manager_group'] = 'Grupo';
+$lang['policy_manager_group_invalid'] = 'Grupo é inválido.';
+$lang['policy_manager_policies'] = 'políticas';
+$lang['policy_manager_policy'] = 'Política';
+$lang['policy_manager_policy_already_exists'] = 'Política já existe.';
+$lang['policy_manager_policy_id_invalid'] = 'ID política é inválido.';
+$lang['policy_manager_policy_name'] = 'Nome da política';
+$lang['policy_manager_policy_name_invalid'] = 'Nome da política é inválido.';
+$lang['policy_manager_policy_not_found'] = 'Política não encontrado.';
+$lang['policy_manager_priority_invalid'] = 'Prioridade é inválido.';
