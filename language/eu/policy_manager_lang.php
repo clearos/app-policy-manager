76c73c438e1b0a9ba1c5004e1ad94c399bb47072
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:29:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers taldea sistemaren erabiltzaile guztien jarraitzeko erabiliko talde berezi bat da. Sortu talde berria, sistemaren erabiltzaile azpimultzo bat zehaztu behar baduzu.';
+$lang['policy_manager_app_description'] = 'Politika Manager aplikazioa partida aplikazioa politikak zabaltzeko talde zehatzei motorra eskaintzen.';
+$lang['policy_manager_app_invalid'] = 'App baliogabea da.';
+$lang['policy_manager_app_name'] = 'politika';
+$lang['policy_manager_description_invalid'] = 'Description baliogabea da.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group baliogabea da.';
+$lang['policy_manager_policies'] = 'Politikak';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika dagoeneko existitzen da.';
+$lang['policy_manager_policy_id_invalid'] = 'Politika ID baliogabea da.';
+$lang['policy_manager_policy_name'] = 'politika izena';
+$lang['policy_manager_policy_name_invalid'] = 'Politika izena baliogabea da.';
+$lang['policy_manager_policy_not_found'] = 'Politika ez da aurkitu.';
+$lang['policy_manager_priority_invalid'] = 'Lehentasuna baliogabea da.';
