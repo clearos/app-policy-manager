f425d14f44cc8987ce3a0e759d2b9e15b36fa1ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers ഗ്രൂപ്പ് സിസ്റ്റത്തിലെ എല്ലാ ഉപയോക്താക്കളെ ഉപയോഗിക്കുന്ന ഒരു പ്രത്യേക സംഘമാണ്. നിങ്ങൾ സിസ്റ്റം ഉപയോക്താക്കളുടെ ഉപസെറ്റ് വ്യക്തമാക്കേണ്ടതുണ്ട് ഒരു പുതിയ ഗ്രൂപ്പ് സൃഷ്ടിക്കുക.';
+$lang['policy_manager_app_description'] = 'നയം മാനേജർ അപ്ലിക്കേഷൻ നിർദ്ദിഷ്ട ഗ്രൂപ്പ് നിർദ്ദിഷ്ട അപ്ലിക്കേഷൻ നയങ്ങൾ വിന്യസിക്കുകയും എഞ്ചിൻ നൽകുന്നു.';
+$lang['policy_manager_app_invalid'] = 'അപ്ലിക്കേഷൻ അസാധുവാണ്.';
+$lang['policy_manager_app_name'] = 'നയം മാനേജർ';
+$lang['policy_manager_description_invalid'] = 'വിവരണം അസാധുവാണ്.';
+$lang['policy_manager_group'] = 'ഗ്രൂപ്പ്';
+$lang['policy_manager_group_invalid'] = 'ഗ്രൂപ്പ് അസാധുവാണ്.';
+$lang['policy_manager_policies'] = 'നയങ്ങള്';
+$lang['policy_manager_policy'] = 'നയം';
+$lang['policy_manager_policy_already_exists'] = 'നയം ഇതിനകം നിലവിലുണ്ട്.';
+$lang['policy_manager_policy_id_invalid'] = 'നയ ID അസാധുവാണ്.';
+$lang['policy_manager_policy_name'] = 'നയത്തിൻറെ പേര്';
+$lang['policy_manager_policy_name_invalid'] = 'നയം പേര് അസാധുവാണ്.';
+$lang['policy_manager_policy_not_found'] = 'നയം കണ്ടെത്തിയില്ല.';
+$lang['policy_manager_priority_invalid'] = 'മുൻഗണന അസാധുവാണ്.';
