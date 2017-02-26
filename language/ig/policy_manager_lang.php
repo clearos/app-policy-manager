127a48b47e6ecf35716270d8890b12f59751abee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'The allusers otu bụ a pụrụ iche otu eji soro niile ọrụ na usoro. Biko ike ọhụrụ otu ma ọ bụrụ na ị chọrọ dee a subset nke ndị ọrụ na usoro.';
+$lang['policy_manager_app_description'] = 'The Policy Manager ngwa na-enye ndị engine maka gbapụ ihe kpọmkwem ngwa atumatu aka kpọmkwem iche iche.';
+$lang['policy_manager_app_invalid'] = 'App bụ ghara ịdị irè.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Description bụ ghara ịdị irè.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group bụ ghara ịdị irè.';
+$lang['policy_manager_policies'] = 'atumatu';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Policy ugbua dị.';
+$lang['policy_manager_policy_id_invalid'] = 'Policy ID bụ ghara ịdị irè.';
+$lang['policy_manager_policy_name'] = 'Policy Name';
+$lang['policy_manager_policy_name_invalid'] = 'Policy aha bụ ghara ịdị irè.';
+$lang['policy_manager_policy_not_found'] = 'Policy ahụghị.';
+$lang['policy_manager_priority_invalid'] = 'Ibu ụzọ bụ ghara ịdị irè.';
