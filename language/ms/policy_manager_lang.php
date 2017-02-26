b2293d7e99f2904689b119de5aa0edd759bd9882
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Kumpulan allusers adalah kumpulan khas yang digunakan untuk mengesan semua pengguna pada sistem. Sila membuat kumpulan baru jika anda perlu untuk menentukan sebahagian kecil pengguna pada sistem.';
+$lang['policy_manager_app_description'] = 'Apl Dasar Pengurus menyediakan enjin untuk menggerakkan dasar aplikasi khusus untuk golongan tertentu.';
+$lang['policy_manager_app_invalid'] = 'App tidak sah.';
+$lang['policy_manager_app_name'] = 'Pengurus Dasar';
+$lang['policy_manager_description_invalid'] = 'Penerangan tidak sah.';
+$lang['policy_manager_group'] = 'Kumpulan';
+$lang['policy_manager_group_invalid'] = 'Kumpulan tidak sah.';
+$lang['policy_manager_policies'] = 'Polisi';
+$lang['policy_manager_policy'] = 'Dasar';
+$lang['policy_manager_policy_already_exists'] = 'Polisi telah wujud.';
+$lang['policy_manager_policy_id_invalid'] = 'ID dasar tidak sah.';
+$lang['policy_manager_policy_name'] = 'Nama Polisi';
+$lang['policy_manager_policy_name_invalid'] = 'Nama dasar tidak sah.';
+$lang['policy_manager_policy_not_found'] = 'Polisi tidak dijumpai.';
+$lang['policy_manager_priority_invalid'] = 'Keutamaan tidak sah.';
