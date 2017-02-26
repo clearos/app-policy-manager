e606a443f6b609fbda9b33f3cd68aa91cbde4e43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Ad inuestigandum est peculiaris coetus universus coetus allusers users ratio. Quaeso quid si definias restant coetus novum users in systematis.';
+$lang['policy_manager_app_description'] = 'A app Policy Manager praebet speciem App Engine et aciemque policies ad specifica coetus.';
+$lang['policy_manager_app_invalid'] = 'App est invalidum.';
+$lang['policy_manager_app_name'] = 'Policy Manager';
+$lang['policy_manager_description_invalid'] = 'Description est invalidum.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group est invalidum.';
+$lang['policy_manager_policies'] = 'Politiae';
+$lang['policy_manager_policy'] = 'Policy';
+$lang['policy_manager_policy_already_exists'] = 'Iam consilium est.';
+$lang['policy_manager_policy_id_invalid'] = 'Id consilium, invalidum est.';
+$lang['policy_manager_policy_name'] = 'Name Policy';
+$lang['policy_manager_policy_name_invalid'] = 'Policy nomen est invalidum.';
+$lang['policy_manager_policy_not_found'] = 'Policy non inveni.';
+$lang['policy_manager_priority_invalid'] = 'Prior est invalidum.';
