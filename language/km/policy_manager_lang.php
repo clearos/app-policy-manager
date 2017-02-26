ea2ca4a67af9f37189040331c2a2a26bb58369a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'ក្រុម allusion គឺជាក្រុមពិសេសប្រើដើម្បីតាមដានអ្នកប្រើទាំងអស់នៅលើប្រព័ន្ធ។ សូមបង្កើតក្រុមថ្មីមួយប្រសិនបើអ្នកត្រូវបញ្ជាក់សំណុំរងនៃអ្នកប្រើប្រាស់មួយនៅលើប្រព័ន្ធបាន។';
+$lang['policy_manager_app_description'] = 'កម្មវិធីគ្រប់គ្រងគោលនយោបាយផ្តល់នូវម៉ាស៊ីនសម្រាប់ការដាក់ពង្រាយគោលនយោបាយកម្មវិធីជាក់លាក់ទៅនឹងក្រុមជាក់លាក់។';
+$lang['policy_manager_app_invalid'] = 'កម្មវិធីមិនត្រឹមត្រូវ។';
+$lang['policy_manager_app_name'] = 'កម្មវិធីគ្រប់គ្រងគោលនយោបាយ';
+$lang['policy_manager_description_invalid'] = 'បរិយាយមិនត្រឹមត្រូវ។';
+$lang['policy_manager_group'] = 'ជាក្រុម';
+$lang['policy_manager_group_invalid'] = 'ជាក្រុមមិនត្រឹមត្រូវ។';
+$lang['policy_manager_policies'] = 'គោលនយោបាយ';
+$lang['policy_manager_policy'] = 'គោលនយោបាយ';
+$lang['policy_manager_policy_already_exists'] = 'គោលនយោបាយរួចទៅហើយ។';
+$lang['policy_manager_policy_id_invalid'] = 'លេខសម្គាល់គោលនយោបាយមិនត្រឹមត្រូវ។';
+$lang['policy_manager_policy_name'] = 'ឈ្មោះគោលនយោបាយ';
+$lang['policy_manager_policy_name_invalid'] = 'ឈ្មោះគោលនយោបាយមិនត្រឹមត្រូវ។';
+$lang['policy_manager_policy_not_found'] = 'រកមិនឃើញគោលនយោបាយ។';
+$lang['policy_manager_priority_invalid'] = 'អាទិភាពគឺមិនត្រឹមត្រូវ។';
