b9ecac771a0dc8b56edb9cac2fcf8d151188dc85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:28:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Die groep allusers is &#39;n spesiale groep gebruik word om al die gebruikers op die stelsel op te spoor. Maak &#39;n nuwe groep as jy nodig het om &#39;n subset van gebruikers spesifiseer op die stelsel.';
+$lang['policy_manager_app_description'] = 'Die beleid Bestuurder app bied die enjin vir die implementering van spesifieke inligting beleid om spesifieke groepe.';
+$lang['policy_manager_app_invalid'] = 'App is ongeldig.';
+$lang['policy_manager_app_name'] = 'beleid Bestuurder';
+$lang['policy_manager_description_invalid'] = 'Beskrywing is ongeldig.';
+$lang['policy_manager_group'] = 'groep';
+$lang['policy_manager_group_invalid'] = 'Groep is ongeldig.';
+$lang['policy_manager_policies'] = 'beleid';
+$lang['policy_manager_policy'] = 'beleid';
+$lang['policy_manager_policy_already_exists'] = 'Beleid bestaan ​​reeds.';
+$lang['policy_manager_policy_id_invalid'] = 'Beleid ID is ongeldig.';
+$lang['policy_manager_policy_name'] = 'beleid Naam';
+$lang['policy_manager_policy_name_invalid'] = 'Beleid naam is ongeldig.';
+$lang['policy_manager_policy_not_found'] = 'Beleid nie gevind nie.';
+$lang['policy_manager_priority_invalid'] = 'Prioriteit is ongeldig.';
