a87d11293319af993b0de17f5bae02a934bccb09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Ko te rōpū allusers ko te rōpū motuhake e whakamahia ana ki te aroturuki i te kaiwhakamahi katoa i runga i te pūnaha. Koa te waihanga i tētahi rōpū hou ki te hiahia koe ki te whakapūtā te huinga o ngā kaiwhakamahi i runga i te pūnaha.';
+$lang['policy_manager_app_description'] = 'E whakarato ana te taupānga Kaiwhakahaere Kaupapa Here te pūkaha mo te deploying kaupapa here taupānga motuhake ki ngā rōpū motuhake.';
+$lang['policy_manager_app_invalid'] = 'He muhu App.';
+$lang['policy_manager_app_name'] = 'Kaiwhakahaere Kaupapahere';
+$lang['policy_manager_description_invalid'] = 'He muhu Whakamārama.';
+$lang['policy_manager_group'] = 'rōpū';
+$lang['policy_manager_group_invalid'] = 'He muhu Rōpū.';
+$lang['policy_manager_policies'] = 'kaupapa here';
+$lang['policy_manager_policy'] = 'Kaupapahere';
+$lang['policy_manager_policy_already_exists'] = 'tīariari kē Kaupapahere.';
+$lang['policy_manager_policy_id_invalid'] = 'He muhu ID kaupapa here.';
+$lang['policy_manager_policy_name'] = 'ingoa Kaupapahere';
+$lang['policy_manager_policy_name_invalid'] = 'He muhu te ingoa kaupapa here.';
+$lang['policy_manager_policy_not_found'] = 'Kaupapahere kahore i kitea.';
+$lang['policy_manager_priority_invalid'] = 'He muhu matua.';
