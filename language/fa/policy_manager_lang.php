5337b1aa6e09629a23a8472cfb7372e3df5cf22c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:36:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'گروه allusers یک گروه خاص استفاده می شود برای پیگیری همه کاربران بر روی سیستم است. لطفا یک گروه جدید ایجاد اگر شما نیاز به مشخص کردن یک زیر مجموعه از کاربران بر روی سیستم.';
+$lang['policy_manager_app_description'] = 'برنامه سیاست مدیریت موتور برای استقرار سیاست های برنامه خاص برای گروه های خاص را فراهم می کند.';
+$lang['policy_manager_app_invalid'] = 'برنامه نامعتبر است.';
+$lang['policy_manager_app_name'] = 'مدیر سیاست';
+$lang['policy_manager_description_invalid'] = 'توضیحات نامعتبر است.';
+$lang['policy_manager_group'] = 'گروه';
+$lang['policy_manager_group_invalid'] = 'گروه نامعتبر است.';
+$lang['policy_manager_policies'] = 'سیاست های';
+$lang['policy_manager_policy'] = 'سیاست';
+$lang['policy_manager_policy_already_exists'] = 'سیاست وجود دارد.';
+$lang['policy_manager_policy_id_invalid'] = 'سیاست ID نامعتبر است.';
+$lang['policy_manager_policy_name'] = 'نام سیاست';
+$lang['policy_manager_policy_name_invalid'] = 'نام سیاست نامعتبر است.';
+$lang['policy_manager_policy_not_found'] = 'سیاست یافت نشد.';
+$lang['policy_manager_priority_invalid'] = 'اولویت نامعتبر است.';
