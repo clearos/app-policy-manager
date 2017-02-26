c3f7cf38ed47728910aa322edd8503145109c25b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'O grupo allusers é un grupo especial usado para controlar todos os usuarios no sistema. Por favor, crear un novo grupo, se precisa especificar un subconxunto de usuarios no sistema.';
+$lang['policy_manager_app_description'] = 'A aplicación Policy Manager ofrece o motor para a implantación de políticas de aplicacións específicas para grupos específicos.';
+$lang['policy_manager_app_invalid'] = 'App non é válido.';
+$lang['policy_manager_app_name'] = 'policy Manager';
+$lang['policy_manager_description_invalid'] = 'Descrición é válido.';
+$lang['policy_manager_group'] = 'grupo';
+$lang['policy_manager_group_invalid'] = 'Grupo é válido.';
+$lang['policy_manager_policies'] = 'políticas';
+$lang['policy_manager_policy'] = 'política';
+$lang['policy_manager_policy_already_exists'] = 'Política xa existe.';
+$lang['policy_manager_policy_id_invalid'] = 'ID política non é válido.';
+$lang['policy_manager_policy_name'] = 'Nome da política';
+$lang['policy_manager_policy_name_invalid'] = 'Nome da política non é válido.';
+$lang['policy_manager_policy_not_found'] = 'Política non atopado.';
+$lang['policy_manager_priority_invalid'] = 'Prioridade non é válido.';
