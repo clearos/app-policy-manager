bb3e3f5fd30b733533f60f8a92a4bdc27197ac21
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:32:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'ALLUSERS समूह एक विशेष प्रणाली पर सभी उपयोगकर्ताओं को ट्रैक करने के लिए इस्तेमाल समूह है। यदि आप सिस्टम पर उपयोगकर्ताओं का एक सबसेट निर्दिष्ट करने की आवश्यकता कृपया एक नया समूह बना।';
+$lang['policy_manager_app_description'] = 'नीति प्रबंधक एप्लिकेशन विशिष्ट समूहों के लिए विशिष्ट एप्लिकेशन नीतियों की तैनाती के लिए इंजन प्रदान करता है।';
+$lang['policy_manager_app_invalid'] = 'अनुप्रयोग अमान्य है।';
+$lang['policy_manager_app_name'] = 'नीति प्रबंधक';
+$lang['policy_manager_description_invalid'] = 'विवरण अमान्य है।';
+$lang['policy_manager_group'] = 'समूह';
+$lang['policy_manager_group_invalid'] = 'समूह अमान्य है।';
+$lang['policy_manager_policies'] = 'नीतियाँ';
+$lang['policy_manager_policy'] = 'नीति';
+$lang['policy_manager_policy_already_exists'] = 'पॉलिसी पहले से मौजूद है।';
+$lang['policy_manager_policy_id_invalid'] = 'नीति आईडी अमान्य है।';
+$lang['policy_manager_policy_name'] = 'पालिसी का नाम';
+$lang['policy_manager_policy_name_invalid'] = 'नीति के नाम अवैध है।';
+$lang['policy_manager_policy_not_found'] = 'पॉलिसी नहीं मिला।';
+$lang['policy_manager_priority_invalid'] = 'वरीयता अवैध है।';
