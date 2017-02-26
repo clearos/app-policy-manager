e05403d78fe58cf55da23986b930e6e5394bab5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:35:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Il-grupp allusers huwa grupp speċjali użat sabiex jittraċċjaw il--utenti kollha fuq is-sistema. Jekk jogħġbok toħloq grupp ġdid jekk għandek bżonn li jispeċifikaw subsett ta &#39;utenti fuq is-sistema.';
+$lang['policy_manager_app_description'] = 'Il-app Manager Politika jipprovdi l-magna għall-iskjerament politiki speċifiċi app għal gruppi speċifiċi.';
+$lang['policy_manager_app_invalid'] = 'App huwa invalidu.';
+$lang['policy_manager_app_name'] = 'Maniġer Politika';
+$lang['policy_manager_description_invalid'] = 'Deskrizzjoni hija invalida.';
+$lang['policy_manager_group'] = 'grupp';
+$lang['policy_manager_group_invalid'] = 'Grupp huwa invalidu.';
+$lang['policy_manager_policies'] = 'politiki';
+$lang['policy_manager_policy'] = 'Politika';
+$lang['policy_manager_policy_already_exists'] = 'diġà jeżisti politika.';
+$lang['policy_manager_policy_id_invalid'] = 'ID-politika hija invalida.';
+$lang['policy_manager_policy_name'] = 'Isem Politika';
+$lang['policy_manager_policy_name_invalid'] = 'Isem politika huwa invalidu.';
+$lang['policy_manager_policy_not_found'] = 'Politika not found.';
+$lang['policy_manager_priority_invalid'] = 'Prijorità hija invalida.';
