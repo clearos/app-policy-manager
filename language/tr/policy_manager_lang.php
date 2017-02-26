fc32b2d83d6bf0660160d672368a48eb6c4164dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers grup sistemdeki tüm kullanıcıları izlemek için kullanılan özel bir gruptur. Eğer sistemde bir kullanıcı alt kümesini belirtmek gerekirse yeni bir grup oluşturun.';
+$lang['policy_manager_app_description'] = 'Policy Manager uygulaması belirli gruplara özgü uygulama ilkeleri uygulamak için motoru sağlar.';
+$lang['policy_manager_app_invalid'] = 'Uygulama geçersiz.';
+$lang['policy_manager_app_name'] = 'Politika Yöneticisi';
+$lang['policy_manager_description_invalid'] = 'Açıklama geçersiz.';
+$lang['policy_manager_group'] = 'grup';
+$lang['policy_manager_group_invalid'] = 'Grup geçersiz.';
+$lang['policy_manager_policies'] = 'İlkeler';
+$lang['policy_manager_policy'] = 'Politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika zaten var.';
+$lang['policy_manager_policy_id_invalid'] = 'Politika kimliği geçersiz.';
+$lang['policy_manager_policy_name'] = 'Poliçe ismi';
+$lang['policy_manager_policy_name_invalid'] = 'Politika adı geçersiz.';
+$lang['policy_manager_policy_not_found'] = 'Politika bulunamadı.';
+$lang['policy_manager_priority_invalid'] = 'Öncelik geçersiz.';
