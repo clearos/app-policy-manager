058999b99056bd41e593ddcd21bdc4763f3cf753
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Awọn allusers Ẹgbẹ jẹ pataki kan ẹgbẹ lo lati orin gbogbo awọn olumulo lori awọn eto. Jọwọ ṣẹda titun kan ẹgbẹ ti o ba nilo lati tokasi a ayosile ti awọn olumulo lori awọn eto.';
+$lang['policy_manager_app_description'] = 'Awọn Afihan Manager app pese awọn engine fun deploying kan pato app imulo to kan pato awọn ẹgbẹ.';
+$lang['policy_manager_app_invalid'] = 'App ni invalid.';
+$lang['policy_manager_app_name'] = 'imulo Manager';
+$lang['policy_manager_description_invalid'] = 'Apejuwe ni invalid.';
+$lang['policy_manager_group'] = 'Group';
+$lang['policy_manager_group_invalid'] = 'Group ni invalid.';
+$lang['policy_manager_policies'] = 'imulo';
+$lang['policy_manager_policy'] = 'eto imulo';
+$lang['policy_manager_policy_already_exists'] = 'Eto imulo tẹlẹ wa.';
+$lang['policy_manager_policy_id_invalid'] = 'Imulo ID ni invalid.';
+$lang['policy_manager_policy_name'] = 'imulo Name';
+$lang['policy_manager_policy_name_invalid'] = 'Imulo orukọ jẹ invalid.';
+$lang['policy_manager_policy_not_found'] = 'Eto imulo ko ba ri.';
+$lang['policy_manager_priority_invalid'] = 'Ni ayo ni invalid.';
