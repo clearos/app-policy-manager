7e5b6e6ce951baea8bfce6e18d96ebda35e39014
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Ang allusers grupo ay isang espesyal na grupo na ginagamit upang subaybayan ang lahat ng mga gumagamit sa sistema. Mangyaring lumikha ng iyong sariling grupo kung kailangan mo upang tukuyin ang isang subset ng mga gumagamit sa sistema.';
+$lang['policy_manager_app_description'] = 'Inilalarawan ng Patakaran sa Manager app ay nagbibigay ng engine para sa deploying mga tiyak na mga patakaran app sa mga tiyak na mga grupo.';
+$lang['policy_manager_app_invalid'] = 'App ay hindi wasto.';
+$lang['policy_manager_app_name'] = 'Patakaran Manager';
+$lang['policy_manager_description_invalid'] = 'Paglalarawan ay hindi wasto.';
+$lang['policy_manager_group'] = 'grupo';
+$lang['policy_manager_group_invalid'] = 'Group ay hindi wasto.';
+$lang['policy_manager_policies'] = 'Mga Patakaran';
+$lang['policy_manager_policy'] = 'patakaran';
+$lang['policy_manager_policy_already_exists'] = 'Patakaran ay mayroon na.';
+$lang['policy_manager_policy_id_invalid'] = 'Patakaran ID ay hindi wasto.';
+$lang['policy_manager_policy_name'] = 'Pangalan Patakaran';
+$lang['policy_manager_policy_name_invalid'] = 'pangalan Patakaran ay hindi wasto.';
+$lang['policy_manager_policy_not_found'] = 'Patakaran hindi natagpuan.';
+$lang['policy_manager_priority_invalid'] = 'Priority ay hindi wasto.';
