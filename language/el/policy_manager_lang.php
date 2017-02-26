bde5fbf37c4ecaa5581134a65692149cbad3b982
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:31:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Η ομάδα allusers είναι μια ειδική ομάδα που χρησιμοποιείται για την παρακολούθηση όλων των χρηστών στο σύστημα. Παρακαλείστε να δημιουργήσετε μια νέα ομάδα, αν θα πρέπει να καθορίσετε ένα υποσύνολο των χρηστών στο σύστημα.';
+$lang['policy_manager_app_description'] = 'Η εφαρμογή Διαχείριση Πολιτικής παρέχει την κινητήρια δύναμη για την ανάπτυξη συγκεκριμένων πολιτικών εφαρμογών σε συγκεκριμένες ομάδες.';
+$lang['policy_manager_app_invalid'] = 'App δεν είναι έγκυρο.';
+$lang['policy_manager_app_name'] = 'Διευθυντής πολιτικής';
+$lang['policy_manager_description_invalid'] = 'Περιγραφή δεν είναι έγκυρο.';
+$lang['policy_manager_group'] = 'Ομάδα';
+$lang['policy_manager_group_invalid'] = 'Ομάδα δεν είναι έγκυρο.';
+$lang['policy_manager_policies'] = 'πολιτικές';
+$lang['policy_manager_policy'] = 'Πολιτική';
+$lang['policy_manager_policy_already_exists'] = 'Πολιτική υπάρχει ήδη.';
+$lang['policy_manager_policy_id_invalid'] = 'ID πολιτική είναι άκυρη.';
+$lang['policy_manager_policy_name'] = 'Όνομα πολιτικής';
+$lang['policy_manager_policy_name_invalid'] = 'Το όνομά της πολιτικής δεν είναι έγκυρο.';
+$lang['policy_manager_policy_not_found'] = 'Πολιτική δεν βρέθηκε.';
+$lang['policy_manager_priority_invalid'] = 'Προτεραιότητα είναι άκυρη.';
