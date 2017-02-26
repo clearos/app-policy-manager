fb8b865a19b58939d2e9ae742162a5ecd3538cfb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'allusers వర్గ వ్యవస్థలో అన్ని వినియోగదారులు ట్రాక్ ఉపయోగించే ఒక ప్రత్యేక గుంపు ఉంది. మీరు సిస్టమ్ వినియోగదారుల ఉపసమితి తెలుపుటకు అవసరం ఉంటే, దయచేసి ఒక కొత్త గుంపు సృష్టించడానికి.';
+$lang['policy_manager_app_description'] = 'విధానం Manager అనువర్తన నిర్దిష్ట సమూహాలకు నిర్దిష్ట అనువర్తన విధానాలను మోహరింపులో ఇంజిన్ అందిస్తుంది.';
+$lang['policy_manager_app_invalid'] = 'అనువర్తనం చెల్లదు.';
+$lang['policy_manager_app_name'] = 'విధానం Manager';
+$lang['policy_manager_description_invalid'] = 'వివరణ చెల్లదు.';
+$lang['policy_manager_group'] = 'గ్రూప్';
+$lang['policy_manager_group_invalid'] = 'గ్రూప్ చెల్లదు.';
+$lang['policy_manager_policies'] = 'నియమాలు';
+$lang['policy_manager_policy'] = 'విధానం';
+$lang['policy_manager_policy_already_exists'] = 'విధానం ఇప్పటికే ఉంది.';
+$lang['policy_manager_policy_id_invalid'] = 'విధానం ID చెల్లదు.';
+$lang['policy_manager_policy_name'] = 'విధానం పేరు';
+$lang['policy_manager_policy_name_invalid'] = 'విధానం పేరు చెల్లనిది.';
+$lang['policy_manager_policy_not_found'] = 'విధానం కనుగొనబడలేదు.';
+$lang['policy_manager_priority_invalid'] = 'ప్రాధాన్య చెల్లదు.';
