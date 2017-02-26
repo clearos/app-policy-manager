da56819a4b1b035e2e9a62cc2c1f01cb6a1c9b82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:34:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Allusers grupė yra speciali grupė naudojama stebėti visus naudotojus dėl sistemos. Prašome sukurti naują grupę, jei reikia nurodyti vartotojų pogrupis sistemoje.';
+$lang['policy_manager_app_description'] = 'Politikos direktorius programa suteikia pagrindą diegti konkrečius programų politiką specialioms grupėms variklį.';
+$lang['policy_manager_app_invalid'] = 'App yra negaliojantis.';
+$lang['policy_manager_app_name'] = 'valdymo blokas';
+$lang['policy_manager_description_invalid'] = 'Aprašymas negalioja.';
+$lang['policy_manager_group'] = 'grupė';
+$lang['policy_manager_group_invalid'] = 'Grupė yra neteisinga.';
+$lang['policy_manager_policies'] = 'politika';
+$lang['policy_manager_policy'] = 'politika';
+$lang['policy_manager_policy_already_exists'] = 'Politika jau egzistuoja.';
+$lang['policy_manager_policy_id_invalid'] = 'Politikos ID negalioja.';
+$lang['policy_manager_policy_name'] = 'politikos pavadinimas';
+$lang['policy_manager_policy_name_invalid'] = 'Politika pavadinimas yra neteisingas.';
+$lang['policy_manager_policy_not_found'] = 'Politikos nerastas.';
+$lang['policy_manager_priority_invalid'] = 'Prioritetas yra neteisinga.';
