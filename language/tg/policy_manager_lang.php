3ae760c78bf5a00deaaf6c2ed08b88480d23b211
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:38:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Гурӯҳи allusers як гурӯҳи махсус истифода бурда мешавад барои пайгирӣ тамоми истифодабарандагони системаи аст. Лутфан, ки гурӯҳи худро таъсис, агар ба шумо лозим аст, то муайян қисми таркибии истифодабарандагони система.';
+$lang['policy_manager_app_description'] = 'Дар барнома сиёсати Менеҷери муҳаррики барои густариши сиёсати барнома мушаххас ба гурӯҳҳои мушаххас таъмин менамояд.';
+$lang['policy_manager_app_invalid'] = 'Барномасозони нодуруст аст.';
+$lang['policy_manager_app_name'] = 'Менеҷер сиёсати';
+$lang['policy_manager_description_invalid'] = 'Тавсифи нодуруст аст.';
+$lang['policy_manager_group'] = 'гурӯҳ';
+$lang['policy_manager_group_invalid'] = 'Гурӯҳи нодуруст аст.';
+$lang['policy_manager_policies'] = 'сиёсати';
+$lang['policy_manager_policy'] = 'сиёсати';
+$lang['policy_manager_policy_already_exists'] = 'Сиёсати аллакай мавҷуд аст.';
+$lang['policy_manager_policy_id_invalid'] = 'Сиёсати ID нодуруст аст.';
+$lang['policy_manager_policy_name'] = 'Номи сиёсати';
+$lang['policy_manager_policy_name_invalid'] = 'Номи сиёсати нодуруст аст.';
+$lang['policy_manager_policy_not_found'] = 'Сиёсати ёфт нашуд.';
+$lang['policy_manager_priority_invalid'] = 'Афзалияти нодуруст аст.';
