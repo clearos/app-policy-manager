3b2905f30aa1f94a0eef50d58afe047e6a323dc3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:33:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'ALLUSERSグループは、システム上のすべてのユーザーを追跡するために使用される特殊なグループです。 あなたは、システム上のユーザーのサブセットを指定する必要がある場合は、新しいグループを作成してください。';
+$lang['policy_manager_app_description'] = 'ポリシーマネージャアプリは、特定のグループに特定のアプリの政策を展開するためのエンジンを提供します。';
+$lang['policy_manager_app_invalid'] = 'アプリは無効です。';
+$lang['policy_manager_app_name'] = 'ポリシーマネージャ';
+$lang['policy_manager_description_invalid'] = '説明が無効です。';
+$lang['policy_manager_group'] = 'グループ';
+$lang['policy_manager_group_invalid'] = 'グループが無効です。';
+$lang['policy_manager_policies'] = 'ポリシー';
+$lang['policy_manager_policy'] = 'ポリシー';
+$lang['policy_manager_policy_already_exists'] = 'ポリシーがすでに存在しています。';
+$lang['policy_manager_policy_id_invalid'] = 'ポリシーIDが無効です。';
+$lang['policy_manager_policy_name'] = 'ポリシー名';
+$lang['policy_manager_policy_name_invalid'] = 'ポリシー名が無効です。';
+$lang['policy_manager_policy_not_found'] = 'ポリシーが見つかりません。';
+$lang['policy_manager_priority_invalid'] = '優先順位は無効です。';
