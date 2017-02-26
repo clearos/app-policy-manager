771735f648d607ea6141139133195ba647f79800
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:39:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/policy_manager_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['policy_manager_allusers_warning'] = 'Các nhóm AllUsers là một nhóm đặc biệt được sử dụng để theo dõi tất cả những người sử dụng trên hệ thống. Hãy tạo ra một nhóm mới nếu bạn cần phải xác định một tập hợp con của người dùng trên hệ thống.';
+$lang['policy_manager_app_description'] = 'Các ứng dụng chính sách quản lý cung cấp động cơ cho việc triển khai các chính sách ứng dụng cụ thể cho các nhóm cụ thể.';
+$lang['policy_manager_app_invalid'] = 'Ứng dụng là không hợp lệ.';
+$lang['policy_manager_app_name'] = 'Quản lý chính sách';
+$lang['policy_manager_description_invalid'] = 'Mô tả không hợp lệ.';
+$lang['policy_manager_group'] = 'Nhóm';
+$lang['policy_manager_group_invalid'] = 'Nhóm là không hợp lệ.';
+$lang['policy_manager_policies'] = 'Chính sách';
+$lang['policy_manager_policy'] = 'chính sách';
+$lang['policy_manager_policy_already_exists'] = 'Chính sách đã tồn tại.';
+$lang['policy_manager_policy_id_invalid'] = 'ID chính sách không hợp lệ.';
+$lang['policy_manager_policy_name'] = 'Tên chính sách';
+$lang['policy_manager_policy_name_invalid'] = 'Tên chính sách không hợp lệ.';
+$lang['policy_manager_policy_not_found'] = 'không tìm thấy chính sách.';
+$lang['policy_manager_priority_invalid'] = 'Ưu tiên là không hợp lệ.';
